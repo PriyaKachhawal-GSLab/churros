9497d84c401f9f0012cfd69883e352e65bac1e7c
jjwyse <josh@cloud-elements.com>
2017-12-12 11:52:34 -0700

    :baby: steps

--- src/test/platform/organizations/rbac.js
+++ src/test/platform/organizations/rbac.js
@@ -23,14 +23,15 @@ suite.forPlatform('Tests privileges restrict API access as expected', test => {
   // make API calls as new user based around those privilege access/restrictions
   // ----
   // revert "org" role back to privileges taken in snapshot
-  let newUser, orgRoleSnapshot;
+  let defaultAccountId, newUser, orgRoleSnapshot;
   before(() => {
     const opts = { qs: { where: 'defaultAccount=true' } };
     return cloud.withOptions(opts).get('/accounts')
       .then(r => {
         expect(r.body.length).to.equal(1);
         const user = { email: `churros+rbac${tools.random()}@churros.com`, firstName: 'frank', lastName: 'ricard', password: 'bingobango' };
-        return cloud.post(`/accounts/${r.body[0].id}/users`, user);
+        defaultAccountId = r.body[0].id;
+        return cloud.post(`/accounts/${defaultAccountId}/users`, user);
       })
       .then(r => {
         expect(r.body.roles).to.have.length.above(0);
@@ -44,8 +45,8 @@ suite.forPlatform('Tests privileges restrict API access as expected', test => {
   });
 
   after(() => {
-    return cloud.delete(`/users/${newUser.id}`, () => true)
-      .then(() => cloud.put(`/organizations/roles/${orgRoleSnapshot.id}`, orgRoleSnapshot, () => true));
+    return cloud.delete(`/users/${newUser.id}`, R.always(true))
+      .then(() => cloud.put(`/organizations/roles/${orgRoleSnapshot.id}`, orgRoleSnapshot, R.always(true)));
   });
 
   const cloudWithUser = () => cloud.withOptions({ headers: { Authorization: `User ${newUser.secret}, Organization ${defaults.secrets().orgSecret}` } });
@@ -102,43 +103,12 @@ suite.forPlatform('Tests privileges restrict API access as expected', test => {
 
     it('should restrict access to viewing formulas without the viewFormulas privilege', () => {
       return removePrivilegeIfNecessary('viewFormulas')
-        .then(() => cloudWithUser().get(`/formulas`, insufficientPrivilegesValidator));
+        .then(() => cloudWithUser().get(`/formulas`, insufficientPrivilegesValidator))
+        .then(() => addPrivilegeIfNecessary('viewFormulas'))
+        .then(() => cloudWithUser().get(`/formulas`));
     });
 
-    it('should restrict access to creating formulas without the createFormula privilege', () => {
-      return removePrivilegeIfNecessary('createFormulas')
-        .then(() => cloudWithUser().post(`/formulas`, DEFAULT_FORMULA, insufficientPrivilegesValidator));
-    });
-
-    it('should restrict access to creating formulas without the createFormulas privilege', () => {
-      return removePrivilegeIfNecessary('createFormulas')
-        .then(() => cloudWithUser().post(`/formulas`, DEFAULT_FORMULA, insufficientPrivilegesValidator));
-    });
-
-    it('should restrict access to editing formulas without the editFormulas privilege', () => {
-      let formulaId;
-      return addPrivilegeIfNecessary('createFormulas')
-        .then(() => cloudWithUser().post(`/formulas`, DEFAULT_FORMULA))
-        .then(r => formulaId = r.body.id)
-        .then(() => removePrivilegeIfNecessary('editFormulas'))
-        .then(() => cloudWithUser().put(`/formulas/${formulaId}`, DEFAULT_FORMULA, insufficientPrivilegesValidator))
-        .then(() => cloudWithUser().delete(`/formulas/${formulaId}`))
-        .catch(() => cloudWithUser().delete(`/formulas/${formulaId}`));
-    });
-
-    it('should restrict access to deleting formulas without the deleteFormulas privilege', () => {
-      let formulaId;
-      return addPrivilegeIfNecessary('createFormulas')
-        .then(() => cloudWithUser().post(`/formulas`, DEFAULT_FORMULA))
-        .then(r => formulaId = r.body.id)
-        .then(() => removePrivilegeIfNecessary('deleteFormulas'))
-        .then(() => cloudWithUser().delete(`/formulas/${formulaId}`, insufficientPrivilegesValidator))
-        .then(() => addPrivilegeIfNecessary('deleteFormulas'))
-        .then(() => cloudWithUser().delete(`/formulas/${formulaId}`))
-        .catch(() => cloudWithUser().delete(`/formulas/${formulaId}`));
-    });
-
-    it('should restrict access to creating a formula instance without the createFormulaInstances privilege', () => {
+    it('should restrict access to creating, editing, and deleting formulas without the necessary privilege', () => {
       let formulaId, formulaInstanceId;
       const cleanup = () => {
         if (!R.isNil(formulaId)) {
@@ -148,17 +118,23 @@ suite.forPlatform('Tests privileges restrict API access as expected', test => {
           cloudWithUser().delete(`/formulas/instances/${formulaInstanceId}`, R.always(true));
         }
       };
-      return addPrivilegeIfNecessary('createFormulas')
+
+      return removePrivilegeIfNecessary('createFormulas')
+        .then(() => cloudWithUser().post(`/formulas`, DEFAULT_FORMULA, insufficientPrivilegesValidator))
+        .then(() => addPrivilegeIfNecessary('createFormulas'))
         .then(() => cloudWithUser().post(`/formulas`, DEFAULT_FORMULA))
         .then(r => formulaId = r.body.id)
+        .then(() => removePrivilegeIfNecessary('editFormulas'))
+        .then(() => cloudWithUser().put(`/formulas/${formulaId}`, DEFAULT_FORMULA, insufficientPrivilegesValidator))
+        .then(() => addPrivilegeIfNecessary('editFormulas'))
+        .then(() => cloudWithUser().put(`/formulas/${formulaId}`, DEFAULT_FORMULA))
         .then(() => removePrivilegeIfNecessary('createFormulaInstances'))
         .then(() => cloudWithUser().post(`/formulas/${formulaId}/instances`, DEFAULT_FORMULA_INSTANCE, insufficientPrivilegesValidator))
         .then(() => addPrivilegeIfNecessary('createFormulaInstances'))
         .then(() => cloudWithUser().post(`/formulas/${formulaId}/instances`, DEFAULT_FORMULA_INSTANCE))
-        .then(r => formulaInstanceId = r.body.id)
-        .then(() => addPrivilegeIfNecessary('deleteFormulas'))
-        .then(cleanup)
-        .catch(cleanup);
+        .then(() => removePrivilegeIfNecessary('deleteFormulas'))
+        .then(() => cloudWithUser().delete(`/formulas/${formulaId}`, insufficientPrivilegesValidator))
+        .then(cleanup);
     });
   });
 
@@ -175,52 +151,16 @@ suite.forPlatform('Tests privileges restrict API access as expected', test => {
       name: `rbac${tools.random()}`
     });
 
-    /* TODO - JJW
-     have not implemented this yet, as from way back in the day we allowed GET /elements to be used without auth.
-     I'm not sure if anyone (or are docs) still rely on that so I didn't remove it.
-     */
-    it('should restrict access to viewing elements without the viewElements privilege');
+    // Postponing, as I'm afraid we have some ppl still calling GET /elements with no creds (marketing site, dev portal)
+    // it('should restrict access to viewing elements without the viewElements privilege', () => {
+    //   const opts = {qs: {page: 1, pageSize: 1}};
+    //   return removePrivilegeIfNecessary('viewElements')
+    //     .then(() => cloudWithUser().withOptions(opts).get(`/elements`, insufficientPrivilegesValidator))
+    //     .then(() => addPrivilegeIfNecessary('viewElements'))
+    //     .then(() => cloudWithUser().withOptions(opts).get(`/elements`));
+    // });
 
-    it('should restrict access to creating elements without the createElements privilege', () => {
-      return removePrivilegeIfNecessary('createElements')
-        .then(() => cloudWithUser().post(`/elements`, DEFAULT_ELEMENT(), insufficientPrivilegesValidator));
-    });
-
-    it('should restrict access to editing an element without the editElements privilege', () => {
-      let elementId;
-      const cleanup = () => {
-        if (R.isNil(elementId)) {
-          cloudWithUser().delete(`/elements/${elementId}`, R.always(true));
-        }
-      };
-
-      return addPrivilegeIfNecessary('createElements')
-        .then(() => cloudWithUser().post(`/elements`, DEFAULT_ELEMENT()))
-        .then(r => elementId = r.body.id)
-        .then(() => removePrivilegeIfNecessary('editElements'))
-        .then(() => cloudWithUser().put(`/elements/${elementId}`, DEFAULT_ELEMENT(), insufficientPrivilegesValidator))
-        .then(() => addPrivilegeIfNecessary('deleteElements'))
-        .then(cleanup);
-    });
-
-    it('should restrict access to deleting elements without the deleteElements privilege', () => {
-      let elementId;
-      const cleanup = () => {
-        if (!R.isNil(elementId)) {
-          cloudWithUser().delete(`/elements/${elementId}`, R.always(true));
-        }
-      };
-
-      return addPrivilegeIfNecessary('createElements')
-        .then(() => cloudWithUser().post(`/elements`, DEFAULT_ELEMENT()))
-        .then(r => elementId = r.body.id)
-        .then(() => removePrivilegeIfNecessary('deleteElements'))
-        .then(() => cloudWithUser().delete(`/elements/${elementId}`, insufficientPrivilegesValidator))
-        .then(() => addPrivilegeIfNecessary('deleteElements'))
-        .then(cleanup);
-    });
-
-    it('should restrict access to creating element instances without the createElementInstances privilege', () => {
+    it('should restrict access to creating, editing, and deleting an element without the editElements privilege', () => {
       let elementId, elementInstanceId;
       const cleanup = () => {
         if (!R.isNil(elementId)) {
@@ -232,14 +172,22 @@ suite.forPlatform('Tests privileges restrict API access as expected', test => {
         }
       };
 
-      return addPrivilegeIfNecessary('createElements')
+      return removePrivilegeIfNecessary('createElements')
+        .then(() => cloudWithUser().post(`/elements`, DEFAULT_ELEMENT(), insufficientPrivilegesValidator))
+        .then(() => addPrivilegeIfNecessary('createElements'))
         .then(() => cloudWithUser().post(`/elements`, DEFAULT_ELEMENT()))
         .then(r => elementId = r.body.id)
+        .then(() => removePrivilegeIfNecessary('editElements'))
+        .then(() => cloudWithUser().put(`/elements/${elementId}`, DEFAULT_ELEMENT(), insufficientPrivilegesValidator))
+        .then(() => addPrivilegeIfNecessary('editElements'))
+        .then(() => cloudWithUser().put(`/elements/${elementId}`, DEFAULT_ELEMENT()))
+        .then(() => removePrivilegeIfNecessary('deleteElements'))
+        .then(() => cloudWithUser().delete(`/elements/${elementId}`, insufficientPrivilegesValidator))
         .then(() => removePrivilegeIfNecessary('createElementInstances'))
         .then(() => cloudWithUser().post(`/elements/${elementId}/instances`, DEFAULT_ELEMENT_INSTANCE(), insufficientPrivilegesValidator))
         .then(() => addPrivilegeIfNecessary('createElementInstances'))
         .then(() => cloudWithUser().post(`/elements/${elementId}/instances`, DEFAULT_ELEMENT_INSTANCE()))
-        .then(r => elementInstanceId = r.body.id)
+        .then(() => addPrivilegeIfNecessary('deleteElements'))
         .then(cleanup);
     });
   });
@@ -261,9 +209,8 @@ suite.forPlatform('Tests privileges restrict API access as expected', test => {
     });
 
     it('should restrict access to creating, editing, and deleting common objects without the proper privileges', () => {
-      const cleanup = () => {
-        return cloudWithUser().delete(`/organizations/objects/${DEFAULT_COMMON_OBJECT.name}/definitions`);
-      };
+      const cleanup = () => cloudWithUser().delete(`/organizations/objects/${DEFAULT_COMMON_OBJECT.name}/definitions`, R.always(true));
+
       return removePrivilegeIfNecessary('createCommonObjects')
         .then(() => cloudWithUser().post(`/organizations/objects/${DEFAULT_COMMON_OBJECT.name}/definitions`, DEFAULT_COMMON_OBJECT, insufficientPrivilegesValidator))
         .then(() => addPrivilegeIfNecessary('createCommonObjects'))
@@ -276,6 +223,125 @@ suite.forPlatform('Tests privileges restrict API access as expected', test => {
         .then(() => cloudWithUser().delete(`/organizations/objects/${DEFAULT_COMMON_OBJECT.name}/definitions`, insufficientPrivilegesValidator))
         .then(() => addPrivilegeIfNecessary('deleteCommonObjects'))
         .then(cleanup);
+    });
+  });
+
+  context('user preferences/branding', () => {
+    const branding = {
+      headerFont: 'Helvetica',
+      headerColor: '#586f75',
+      bodyFont: 'Helvetica',
+      bodyColor: '#aedce7',
+      themePrimaryColor: '#586f75',
+      themeSecondaryColor: '#aedce7',
+      themeHighlightColor: '#468499',
+      buttonPrimaryBackgroundColor: '#c64',
+      buttonPrimaryTextColor: '#aedce7',
+      buttonSecondaryBackgroundColor: '#aedce7',
+      buttonSecondaryTextColor: '#cc6649',
+      buttonDeleteBackgroundColor: '#eee',
+      buttonDeleteTextColor: '#FFF',
+      logoBackgroundColor: '#ffffff',
+      topBarBackgroundColor: '#aedce7',
+      navigationBackgroundColor: '#aedce7',
+      contextBackgroundColor: '#aedce7',
+    };
+
+    it('should restrict access to updating the organization branding information with the proper privilege', () => {
+      return removePrivilegeIfNecessary('manageUiBranding')
+        .then(() => cloudWithUser().delete(`/organizations/branding`, insufficientPrivilegesValidator))
+        .then(() => cloudWithUser().put(`/organizations/branding`, branding, insufficientPrivilegesValidator))
+        .then(() => addPrivilegeIfNecessary('manageUiBranding'))
+        .then(() => cloudWithUser().put(`/organizations/branding`, branding))
+        .then(() => cloudWithUser().delete(`/organizations/branding`));
+    });
+  });
+
+  context('account management', () => {
+    const viewTest = (level, levelPrivilegeSuffix) => {
+      return removePrivilegeIfNecessary(`viewUsers${levelPrivilegeSuffix}`)
+        .then(() => cloudWithUser().get(`/${level}/${defaultAccountId}/users`, insufficientPrivilegesValidator))
+        .then(() => addPrivilegeIfNecessary(`viewUsers${levelPrivilegeSuffix}`))
+        .then(() => cloudWithUser().get(`/${level}/${defaultAccountId}/users`));
+    };
+
+    it('should restrict viewing account users without the proper privilege', () => {
+      return viewTest('acounts', 'Account');
+    });
+
+    it('should restrict creating, editing, and deleting a new account user without the proper privileges', () => {
+      const user = { email: `churros+rbac${tools.random()}@churros.com`, firstName: 'joseph-account', lastName: 'pulaski', password: 'bingobango' };
+      let userId;
+      return removePrivilegeIfNecessary('addUsersAccount')
+        .then(() => cloudWithUser().post(`/accounts/${defaultAccountId}/users`, user, insufficientPrivilegesValidator))
+        .then(() => addPrivilegeIfNecessary('addUsersAccount'))
+        .then(() => cloudWithUser().post(`/accounts/${defaultAccountId}/users`, user))
+        .then(r => userId = r.body.id)
+        .then(() => removePrivilegeIfNecessary('editUsersAccount'))
+        .then(() => cloudWithUser().patch(`/accounts/${defaultAccountId}/users/${userId}`, user, insufficientPrivilegesValidator))
+        .then(() => addPrivilegeIfNecessary('editUsersAccount'))
+        .then(() => cloudWithUser().patch(`/accounts/${defaultAccountId}/users/${userId}`, user))
+        .then(() => removePrivilegeIfNecessary('deleteUsersAccount'))
+        .then(() => cloudWithUser().delete(`/accounts/${defaultAccountId}/users/${userId}`, insufficientPrivilegesValidator))
+        .then(() => addPrivilegeIfNecessary('deleteUsersAccount'))
+        .then(() => cloudWithUser().delete(`/accounts/${defaultAccountId}/users/${userId}`));
+    });
+
+    it('should restrict viewing organization users without the proper privilege', () => {
+      return viewTest('organizations', 'Org');
+    });
+
+    it('should restrict creating, editing, and eleting a new organization user without the proper privileges', () => {
+      const user = { email: `churros+rbac${tools.random()}@churros.com`, firstName: 'joseph-organization', lastName: 'pulaski', password: 'bingobango' };
+      let userId;
+      return removePrivilegeIfNecessary('addUsersOrg')
+        .then(() => cloudWithUser().post(`/organizations/users`, user, insufficientPrivilegesValidator))
+        .then(() => addPrivilegeIfNecessary('addUsersOrg'))
+        .then(() => cloudWithUser().post(`/organizations/users`, user))
+        .then(r => userId = r.body.id)
+        .then(() => removePrivilegeIfNecessary('editUsersOrg'))
+        .then(() => cloudWithUser().patch(`/organizations/users/${userId}`, user, insufficientPrivilegesValidator))
+        .then(() => addPrivilegeIfNecessary('editUsersOrg'))
+        .then(() => cloudWithUser().patch(`/organizations/users/${userId}`, user))
+        .then(() => removePrivilegeIfNecessary('deleteUsersOrg'))
+        .then(() => cloudWithUser().delete(`/organizations/users/${userId}`, insufficientPrivilegesValidator))
+        .then(() => addPrivilegeIfNecessary('deleteUsersOrg'))
+        .then(() => cloudWithUser().delete(`/organizations/users/${userId}`));
+    });
+  });
+
+  context('logs', () => {
+    it('should restrict access to any audit log data without the proper privilege', () => {
+      const customValidator = r => {
+        // if a user does not have ES setup/running locally they'll get a 500 here.  That's kind of fine,
+        // cause it at least shows they have the privilege to go get the audit logs.
+        const statusCode = r.response.statusCode;
+        expect(statusCode).to.be.oneOf([200, 500]);
+      };
+      return removePrivilegeIfNecessary('viewLogs')
+        .then(() => cloudWithUser().get(`/audit-logs`, insufficientPrivilegesValidator))
+        .then(() => addPrivilegeIfNecessary('viewLogs'))
+        .then(() => cloudWithUser().get(`/audit-logs`, customValidator));
+    });
+  });
+
+  context('security', () => {
+    const passwordPolicy = { minLength: 8, minUppercase: 0, minLowercase: 0, minNumbers: 0, minSymbols: 0, symbolCharacterSet: '!@#$%^&*()', previousPasswordCount: 0, expirationDays: 1000, };
+    it('should restrict modifying the password policy without the proper privilege', () => {
+      return removePrivilegeIfNecessary('modifySecurity')
+        .then(() => cloudWithUser().put(`/organizations/password-policies`, passwordPolicy, insufficientPrivilegesValidator))
+        .then(() => addPrivilegeIfNecessary('modifySecurity'))
+        .then(() => cloudWithUser().put(`/organizations/password-policies`, passwordPolicy))
+        .then(() => cloudWithUser().delete(`/organizations/password-policies`));
+    });
+  });
+
+  context('roles', () => {
+    it('should restrict viewing roles if the user does not have the proper privilege for configuring organization roles', () => {
+      return removePrivilegeIfNecessary('configureRoles')
+        .then(() => cloudWithUser().get(`/organizations/roles`, insufficientPrivilegesValidator))
+        .then(() => addPrivilegeIfNecessary('configureRoles'))
+        .then(() => cloudWithUser().get(`/organizations/roles`));
     });
   });
 });
