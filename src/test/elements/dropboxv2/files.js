49e1729d1e3867954e9056bfb94f6b18e920c011
seshuede <seshuede@gmail.com>
2017-12-17 17:24:58 -0600

    added testcase revisons by path

--- src/test/elements/dropboxv2/files.js
+++ src/test/elements/dropboxv2/files.js
@@ -5,24 +5,35 @@ const suite = require('core/suite');
 const faker = require('faker');
 const expect = require('chakram').expect;
 
-suite.forElement('documents','files',(test) => {
+suite.forElement('documents', 'files', (test) => {
 
   let jpgFile = __dirname + '/assets/brady.jpg';
-  var jpgFileBody,revisionId;
+  var jpgFileBody, revisionId;
   let directoryPath = faker.random.word();
 
-  before(() => cloud.withOptions({ qs : { path : `/${directoryPath}/brady.jpg` }}).postFile(test.api, jpgFile)
-  .then(r => jpgFileBody = r.body));
+  before(() => cloud.withOptions({ qs: { path: `/${directoryPath}/brady.jpg` } }).postFile(test.api, jpgFile)
+    .then(r => jpgFileBody = r.body));
 
   after(() => cloud.delete(`${test.api}/${jpgFileBody.id}`));
 
-  it('should allow GET /files/:id/revisions', () => {
-      return cloud.get(`${test.api}/${jpgFileBody.id}/revisions`)
+  it('it should allow RS for documents/files/:id/revisions', () => {
+    return cloud.get(`${test.api}/${jpgFileBody.id}/revisions`)
       .then(r => {
         expect(r.body.filter(obj => obj.fileName === 'brady.jpg')).to.not.be.empty;
         revisionId = r.body[0].id;
-        })
+      })
       .then(() => cloud.get(`${test.api}/${jpgFileBody.id}/revisions/${revisionId}`))
       .then(r => expect(r.body.fileName).to.equal("brady.jpg"));
   });
+
+  it('it should allow RS for documents/files/revisions by path', () => {
+    return cloud.withOptions({ qs: { path: `/${directoryPath}/brady.jpg` } }).get(`${test.api}/revisions`)
+      .then(r => {
+        expect(r.body.filter(obj => obj.fileName === 'brady.jpg')).to.not.be.empty;
+        revisionId = r.body[0].id;
+      })
+      .then(() => cloud.withOptions({ qs: { path: `/${directoryPath}/brady.jpg` } }).get(`${test.api}/revisions/${revisionId}`))
+      .then(r => expect(r.body.fileName).to.equal("brady.jpg"));
+  });
+
 });
