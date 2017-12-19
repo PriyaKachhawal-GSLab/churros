5aa7c439915c46d22601a5336942a09069edaa99
seshuede <seshuede@gmail.com>
2017-12-18 22:16:06 -0600

    Modified Validation as per Review comments

--- src/test/elements/dropboxbusinessv2/files.js
+++ src/test/elements/dropboxbusinessv2/files.js
@@ -2,40 +2,31 @@
 
 const cloud = require('core/cloud');
 const suite = require('core/suite');
-const faker = require('faker');
 const props = require('core/props');
-const expect = require('chakram').expect;
+const tools = require('core/tools');
 
 suite.forElement('documents','files',(test) => {
 
   let jpgFile = __dirname + '/assets/brady.jpg';
   var jpgFileBody,revisionId;
-  let directoryPath = faker.random.word();
   let memberId = props.getForKey('dropboxbusinessv2', 'username');
+  let query = { path: `/brady-${tools.randomStr('abcdefghijklmnopqrstuvwxyz1234567890', 10)}.jpg` };
 
-  before(() => cloud.withOptions({ qs : { path : `/${directoryPath}/brady.jpg` , headers: { "Elements-As-Team-Member": memberId } }}).postFile(test.api, jpgFile)
+  before(() => cloud.withOptions({ qs : query , headers: { "Elements-As-Team-Member": memberId }}).postFile(test.api, jpgFile)
   .then(r => jpgFileBody = r.body));
 
   after(() => cloud.withOptions({ headers: { "Elements-As-Team-Member": memberId } }).delete(`${test.api}/${jpgFileBody.id}`));
 
   it('it should allow RS for documents/files/:id/revisions', () => {
       return cloud.withOptions({ headers: { "Elements-As-Team-Member": memberId } }).get(`${test.api}/${jpgFileBody.id}/revisions`)
-      .then(r => {
-        expect(r.body.filter(obj => obj.fileName === 'brady.jpg')).to.not.be.empty;
-        revisionId = r.body[0].id;
-        })
-      .then(() => cloud.withOptions({ headers: { "Elements-As-Team-Member": memberId } }).get(`${test.api}/${jpgFileBody.id}/revisions/${revisionId}`))
-      .then(r => expect(r.body.fileName).to.equal("brady.jpg"));
+      .then(r => revisionId = r.body[0].id)
+      .then(() => cloud.withOptions({ headers: { "Elements-As-Team-Member": memberId } }).get(`${test.api}/${jpgFileBody.id}/revisions/${revisionId}`));
   });
 
   it('it should allow RS for documents/files/revisions by path', () => {
-      return cloud.withOptions({ qs: { path: `/${directoryPath}/brady.jpg`} , headers: { "Elements-As-Team-Member": memberId }}).get(`${test.api}/revisions`)
-      .then(r => {
-        expect(r.body.filter(obj => obj.fileName === 'brady.jpg')).to.not.be.empty;
-        revisionId = r.body[0].id;
-      })
-      .then(() => cloud.withOptions({ qs: { path: `/${directoryPath}/brady.jpg`} , headers: { "Elements-As-Team-Member": memberId }}).get(`${test.api}/revisions/${revisionId}`))
-      .then(r => expect(r.body.fileName).to.equal("brady.jpg"));
+      return cloud.withOptions({ qs: query , headers: { "Elements-As-Team-Member": memberId }}).get(`${test.api}/revisions`)
+      .then(r => revisionId = r.body[0].id)
+      .then(() => cloud.withOptions({ qs: query , headers: { "Elements-As-Team-Member": memberId }}).get(`${test.api}/revisions/${revisionId}`));
   });
 
 });
