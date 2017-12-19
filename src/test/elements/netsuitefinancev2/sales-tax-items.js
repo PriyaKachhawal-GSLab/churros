15c4011366448c906035c88f40ed10810d22e260
seshuede <seshuede@gmail.com>
2017-12-19 12:59:02 -0600

    Validate Resopnse

--- src/test/elements/netsuitefinancev2/sales-tax-items.js
+++ src/test/elements/netsuitefinancev2/sales-tax-items.js
@@ -1,8 +1,11 @@
 'use strict';
 
 const suite = require('core/suite');
-
+const expect = require('chakram').expect;
 suite.forElement('finance', 'sales-tax-items', (test) => {
     test.withOptions({ qs: { page: 1, pageSize: 5 } }).should.supportPagination();
-    test.withOptions({ qs: { where: 'lastModifiedDate >= \'2016-08-05T09:35:38Z\'' } }).should.return200OnGet();
+    test.withApi(test.api)
+        .withValidation(r => expect(r.body[0]).to.contain.key('internalId'))
+        .withName('should allow GET /sales-tax-items')
+        .should.return200OnGet();
 });
