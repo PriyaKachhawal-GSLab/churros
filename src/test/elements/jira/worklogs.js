b582a94196a4c7dcab79cbcfb56dbaf73adb470b
MadhuriKadam-GSLab <madhuri.kadam@gslab.com>
2017-12-20 09:22:02 +0000

    updated the worklog test

--- src/test/elements/jira/worklogs.js
+++ src/test/elements/jira/worklogs.js
@@ -5,5 +5,9 @@ const suite = require('core/suite');
 suite.forElement('helpdesk', 'worklogs', (test) => {
  
 test.withOptions({ qs: { where: `updated='true'` }}).should.supportS();
+test.withOptions({ qs: { where: `deleted='true'` }}).should.supportS();
+test.withOptions({ qs: { where: `id in (10300)` }}).should.supportS();
+
+test.withOptions({ qs: { where: `updated='true'` }}).should.supportPagination();
 
 });
