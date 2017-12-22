801c58df9122fac7128ddde87abab49affbd58d6
Atul Barve <atul@cloud-elements.com>
2017-12-22 12:45:57 -0600

    Added and Fixed Churros for QBD. make sure u run them with --sync flag

--- src/test/elements/quickbooksonprem/payments.js
+++ src/test/elements/quickbooksonprem/payments.js
@@ -7,8 +7,16 @@ const payload = tools.requirePayload(`${__dirname}/assets/payments.json`);
 const updatePayload = { "Memo": tools.random(), "TotalAmount": "136.00" };
 
 suite.forElement('finance', 'payments', (test) => {
+  let invoiceTxnId, invoiceCustomerRef;
+  before(() => cloud.get(`/invoices`)
+    .then(r => {
+      invoiceTxnId = r.body[0].id;
+      invoiceCustomerRef = r.body[0].CustomerRef.ListID;
+    }));
   it('should support CRUDS and Ceql searching for /hubs/finance/payments', () => {
     let id;
+    payload.CustomerRef.ListID = invoiceCustomerRef;
+    payload.AppliedToTxn[0].TxnID = invoiceTxnId;
     return cloud.post(test.api, payload)
       .then(r => {
         id = r.body.id;
