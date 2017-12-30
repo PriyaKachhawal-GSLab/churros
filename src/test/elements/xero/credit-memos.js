1b817a126c9075d9fcea4522d9d7cad979ee06da
Taylor Clark <taylor@cloud-elements.com>
2017-12-30 12:43:46 -0600

    moved wrapper to a before/after

--- src/test/elements/xero/credit-memos.js
+++ src/test/elements/xero/credit-memos.js
@@ -11,57 +11,54 @@ suite.forElement('finance', 'credit-memos', (test) => {
         // to avoid rate limit errors
         setTimeout(done, 5000);
     });
-    test.should.supportPagination();
-    
-    const contactWrap = (cb) => {
-      let contactId;
-      let contactPayload = tools.requirePayload(`${__dirname}/assets/contact.json`);    
 
-      return cloud.post('/contacts', contactPayload)
-      .then(r => contactId = r.body.ContactID)
-      .then(() => cb(contactId))
-      .then(() => cloud.delete(`/contacts/${contactId}`));
-    };
+    test.should.supportPagination();
+
+    let contactId;
+
+    before(() => {     
+      let contactPayload = tools.requirePayload(`${__dirname}/assets/contact.json`);    
+      cloud.post('/contacts', contactPayload)
+        .then(r => contactId = r.body.ContactID);
+    });
+
+    after(() => 
+      cloud.delete(`/contacts/${contactId}`
+    ));
+    
 
     it('should support CRUDS for /credit-memos', () => {
-      const cb = (contactId) => {
-        let creditMemoPayload = tools.requirePayload(`${__dirname}/assets/credit-memo.json`);
-        let creditMemoUpdate = {Reference: lorem.words()};
-        creditMemoPayload.Contact.ContactID = contactId;
-        
-        return cloud.post(test.api, creditMemoPayload)
-          .then((r) => cloud.get(`${test.api}/${r.body.CreditNoteID}`))
-          .then((r) => cloud.patch(`${test.api}/${r.body.CreditNoteID}`, creditMemoUpdate))
-          .then((r) => expect(r.body.Reference).to.equal(creditMemoUpdate.Reference))
-          .then(() => cloud.withOptions({qs:{where: `Reference='${creditMemoUpdate.Reference}'`}}).get(test.api))
-          .then((r) => {
-            expect(r.body.length).to.equal(1);
-            cloud.delete(`${test.api}/${r.body[0].CreditNoteID}`);
-          });
-        };
-        return contactWrap(cb);
+      let creditMemoPayload = tools.requirePayload(`${__dirname}/assets/credit-memo.json`);
+      let creditMemoUpdate = {Reference: lorem.words()};
+      creditMemoPayload.Contact.ContactID = contactId;
+      
+      return cloud.post(test.api, creditMemoPayload)
+        .then((r) => cloud.get(`${test.api}/${r.body.CreditNoteID}`))
+        .then((r) => cloud.patch(`${test.api}/${r.body.CreditNoteID}`, creditMemoUpdate))
+        .then((r) => expect(r.body.Reference).to.equal(creditMemoUpdate.Reference))
+        .then(() => cloud.withOptions({qs:{where: `Reference='${creditMemoUpdate.Reference}'`}}).get(test.api))
+        .then((r) => {
+          expect(r.body.length).to.equal(1);
+          cloud.delete(`${test.api}/${r.body[0].CreditNoteID}`);
+        });
     });
 
     it('should support POST /credit-memos/:id/allocations', () => {
-      const cb = (contactId) => {
-        let allocationCreditMemo = tools.requirePayload(`${__dirname}/assets/credit-memo-POST-allocations.json`);
-        let allocationInvoice = tools.requirePayload(`${__dirname}/assets/invoice-POST-allocations.json`);
-        let payload = tools.requirePayload(`${__dirname}/assets/credit-memo-allocation.json`);
-        let creditMemoId, invoiceId;
-        allocationCreditMemo.Contact.ContactID = contactId;
-        allocationInvoice.Contact.ContactID = contactId;
-        return cloud.post(test.api, allocationCreditMemo)
-          .then(r => creditMemoId = r.body.CreditNoteID)
-          .then(() => cloud.post('/invoices', allocationInvoice))
-          .then(r => invoiceId = r.body.id)
-          .then(() => {
-            payload.Invoice.InvoiceID = invoiceId;
-            payload.Date = Date.now();
-          })
-          .then(() => cloud.post(`${test.api}/${creditMemoId}/allocations`, payload));
-      };
-
-      return contactWrap(cb);
+      let allocationCreditMemo = tools.requirePayload(`${__dirname}/assets/credit-memo-POST-allocations.json`);
+      let allocationInvoice = tools.requirePayload(`${__dirname}/assets/invoice-POST-allocations.json`);
+      let payload = tools.requirePayload(`${__dirname}/assets/credit-memo-allocation.json`);
+      let creditMemoId, invoiceId;
+      allocationCreditMemo.Contact.ContactID = contactId;
+      allocationInvoice.Contact.ContactID = contactId;
+      return cloud.post(test.api, allocationCreditMemo)
+        .then(r => creditMemoId = r.body.CreditNoteID)
+        .then(() => cloud.post('/invoices', allocationInvoice))
+        .then(r => invoiceId = r.body.id)
+        .then(() => {
+          payload.Invoice.InvoiceID = invoiceId;
+          payload.Date = Date.now();
+        })
+        .then(() => cloud.post(`${test.api}/${creditMemoId}/allocations`, payload));
     });
 
 });
