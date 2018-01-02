bd3029844659b4968969a6ab3a20d07ab7730051
MitilaGarud-GSLab <mithila.garud@gslab.com>
2018-01-02 16:42:04 +0530

    Added empty check on response of GET all pages

--- src/test/elements/facebookleadads/pages.js
+++ src/test/elements/facebookleadads/pages.js
@@ -8,9 +8,14 @@ suite.forElement('marketing', 'pages', null, (test) => {
   test.should.supportSr();
   it('should allow SR for /hubs/marketing/pages', () => {
     return cloud.get(test.api)
-      .then(r => pageId = r.body[0].id)
-      .then(r => cloud.get(`${test.api}/${pageId}`))
-      .then(r => cloud.withOptions({ qs: { fields: 'id,name' } }).get(`${test.api}/${pageId}`));
+      .then(r => {
+        if (r.body.length <= 0) {
+          return;
+        } else {
+          pageId = pageId = r.body[0].id;
+          return cloud.withOptions({ qs: { fields: 'id,name' } }).get(`${test.api}/${pageId}`);
+        }
+      });
   });
 
   it(`should allow CR for ${test.api}/${pageId}/subscribed-apps`, () => {
