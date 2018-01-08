d360e597cf1131e46317fe1173015af2581d700d
madhuri.kadam <madhuri.kadam@gslab.com>
2018-01-08 16:09:02 +0530

    added the checking accounts

--- src/test/elements/intacct/bills-payments.js
+++ src/test/elements/intacct/bills-payments.js
@@ -6,31 +6,18 @@ const expect = require('chakram').expect;
 
 
 suite.forElement('finance', 'bills-payments', null, (test) => {
-  
+
  let id;
-  it(`should allow GET for ${test.api}`, () => {
-  return cloud.get(`${test.api}`)
-    .then(r => {
-      if (r.body.length <= 0) {
-        return;
-      } else {
-        id = r.body[0].RECORDNO;
-        return cloud.get(`${test.api}/${id}`);
-      }
-    });});
-  if (id !== null) {
-
-
-
- return cloud.withOptions({ qs: { where: 'whenmodified>\'08/13/2016 05:26:37\'' } })
-        .get(test.api)
-        .then((r) => {
-          expect(r).to.statusCode(200);
-          const validValues = r.body.filter(obj =>
-            obj.whenmodified >= '08/13/2016 05:26:37');
-          expect(validValues.length).to.equal(r.body.length);
-        });
-
-}
+ test.should.supportSr();
+ test
+    .withOptions({ qs: { where: `WHENMODIFIED > '06/23/2013 14:25:17'` } })
+    .withName('should support Ceql date search')
+    .withValidation(r => {
+      expect(r).to.statusCode(200);
+      const validValues = r.body.filter(obj => obj.WHENMODIFIED >= '06/23/2013 14:25:17');
+      expect(validValues.length).to.equal(r.body.length);
+    })
+    .should.return200OnGet();
+    test.should.supportNextPagePagination(1,false);
 
 });
