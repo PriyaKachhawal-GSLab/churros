0b4201459c231f20c30ef888b58220070db24006
Chinna <chinnapave@gmail.com>
2018-01-13 12:51:31 -0600

    Fixed the failing curros for accounts

--- src/test/elements/freshdeskv2/accounts.js
+++ src/test/elements/freshdeskv2/accounts.js
@@ -2,9 +2,23 @@
 
 const suite = require('core/suite');
 const tools = require('core/tools');
-const payload = tools.requirePayload(`${__dirname}/assets/accounts.json`);
+const cloud = require('core/cloud');
+const expect = require('chakram').expect;
+const accountsPayload = tools.requirePayload(`${__dirname}/assets/accounts.json`);
+const accountsPayloadUpdate = tools.requirePayload(`${__dirname}/assets/accountsUpdate.json`);
 
-suite.forElement('helpdesk', 'accounts', { payload: payload }, (test) => {
-  test.should.supportCruds();
+suite.forElement('helpdesk', 'accounts', (test) => {
   test.should.supportPagination();
+
+  it('should allow CRUDS for employees', () => {
+    let aId;
+    return cloud.post(test.api, accountsPayload)
+      .then(r => aId = r.body.customer.id)
+      .then(r => cloud.get(`${test.api}/${aId}`))
+      .then(r => cloud.patch(`${test.api}/${aId}`, accountsPayloadUpdate))
+      .then(r => cloud.delete(`${test.api}/${aId}`))
+      .then(r => cloud.get(test.api))
+      .then(r => expect(r.body.filter(obj => obj.name !== '')).to.not.be.empty);
+  });
+
 });
