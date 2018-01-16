a666215747e4a9e6483d0da8ee1a1369738d8cdc
Martin Brandt <martin@cloud-elements.com>
2018-01-16 14:49:05 -0700

    added test for element objects hydrating resources

--- src/test/platform/elements/resources.js
+++ src/test/platform/elements/resources.js
@@ -6,6 +6,7 @@ const common = require('./assets/common.js');
 const schema = require('./assets/element.resource.schema.json');
 const listSchema = require('./assets/element.resources.schema.json');
 const resourcePayload = require('./assets/element.resource.payload.json');
+const expect = require('chakram').expect;
 
 const opts = { payload: common.genResource({}), schema: schema };
 
@@ -26,6 +27,14 @@ suite.forPlatform('elements/resources', opts, (test) => {
 
   it('should support CRUD by key with sample data and other optional fields', () => common.crudsResource(keyUrl, schema, listSchema, common.genResource({}), common.genResource({ description: "An updated Churros resource", response: { sampleData: "{\"key\": \"value\"}"}, operationId: "getChurrosById" })));
   it('should support CRUD by ID with sample data and other optional fields', () => common.crudsResource(idUrl, schema, listSchema, common.genResource({}), common.genResource({ description: "An updated Churros resource", response: { sampleData :"{\"key\": \"value\"}"}, operationId: "getChurrosByKey" })));
+
+  it('should return resources associated with an object', () =>{
+    let objectId;
+    return cloud.post(`elements/${element.id}/resources`, resourcePayload)
+    .then(r => objectId = r.body.object.id)
+    .then(r => cloud.withOptions({qs: {hydrate: true}}).get(`elements/${element.id}/objects/${objectId}`))
+    .then(r => expect(r.body.resources).to.have.length(1));
+  });
 
   after(() => cloud.delete(`elements/${element.key}`));
 });
