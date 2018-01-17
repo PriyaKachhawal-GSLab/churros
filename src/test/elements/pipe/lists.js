dcf89dd104214ae910ed374a11539c614b7d331b
sarahhealey <sarahdhealey@gmail.com>
2018-01-17 11:18:47 -0700

    fixed errors outlined by circleci

--- src/test/elements/pipe/lists.js
+++ src/test/elements/pipe/lists.js
@@ -4,26 +4,12 @@ const suite = require('core/suite');
 const tools = require('core/tools');
 const cloud = require('core/cloud');
 
-const contactPayload = {
-  "person": {
-    "lastName": tools.randomStr(),
-    "firstName": tools.randomStr(),
-    "email": tools.randomEmail()
-  }
-};
-
-const listContactPayload = [{
-  "person": {
-    "id": tools.randomInt()
-  }
-}];
-
 suite.forElement('helpdesk', 'lists', null, (test) => {
   it('should allow SR for /lists', () => {
-    let id, contactId, objectName;
+    let id;
     return cloud.get(test.api)
       .then(r => id = r.body[0].id)
       .then(r => cloud.get(`${test.api}/${id}`))
-      .then(r => cloud.get(`${test.api}/${id}/search`))
+      .then(r => cloud.get(`${test.api}/${id}/search`));
   });
 });
