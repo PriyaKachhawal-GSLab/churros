9717a09cae60fef4f2e73b238e869c514ac51ba4
sarahhealey <sarahdhealey@gmail.com>
2018-01-17 11:10:15 -0700

    fixed circle ci linting errors

--- src/test/elements/mailchimpv3/campaigns.js
+++ src/test/elements/mailchimpv3/campaigns.js
@@ -3,7 +3,6 @@
 const suite = require('core/suite');
 const payload = require('core/tools').requirePayload(`${__dirname}/assets/campaigns.json`);
 const cloud = require('core/cloud');
-const tools = require('core/tools');
 
 const updatePayload = () => ({
   "recipients": {
@@ -48,7 +47,7 @@ suite.forElement('marketing', 'campaigns', { payload: payload }, (test) => {
       .then(r => cloud.post(`${test.api}/${campaignId}/comments`, commentsPayload()))
       .then(r => cloud.withOptions({qs: { page: 1, pageSize: 1 } }).get(`${test.api}/${campaignId}/comments`))
       .then(r => cloud.delete(`${test.api}/${campaignId}/comments/${commentId}`))
-      .then(r => cloud.delete(`${test.api}/${campaignId}`))
+      .then(r => cloud.delete(`${test.api}/${campaignId}`));
   });
 /*Need to skip because the campaign/{id}/email-activities/{emailId} is read only
 Since there is no post to this endpoint, the values below are hardcoded for initial testing
@@ -57,7 +56,7 @@ This test is just for reference to future-proof in case the endpoint breaks down
     let campaignId = '03747e516a';
     let emailId = -1;
     return cloud.get(`${test.api}/${campaignId}/email-activity`)
-      .then (r => emailId = r.body.emails[0].email_id)
+      .then (r => emailId = r.body.emails[0].email_id);
       .then(r => cloud.get(`${test.api}/${campaignId}/email-activity/${emailId}`))
   });
 });
