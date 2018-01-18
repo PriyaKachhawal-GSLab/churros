59fbe36ae5e471bf762bfdc57c822e3ab51bf134
rahul gupta <rahul.gupta@gslab.com>
2018-01-18 18:01:26 +0530

    Searchable netsuiteerpv2

---
+++ src/test/elements/netsuiteerpv2/searchable.js
@@ -0,0 +1,14 @@
+'use strict';
+
+const suite = require('core/suite');
+const model = require('core/model');
+
+let bills = '/hubs/erp/bills';
+
+suite.forElement('erp', 'searchable', (test) => {  
+  it('should allow find searchable fields hubs/erp/accounts', () => {
+     return model.searchableFields('hubs/erp/accounts')
+    // .then(r => model.searchableFields(bills));    
+    //return model.searchableFields(bills);    
+  });
+});
