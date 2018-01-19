3b2c7815f33f8c25b4af8c379645a55a30b885d6
madhuri.kadam <madhuri.kadam@gslab.com>
2018-01-19 10:39:40 +0530

    updated the review comments

--- src/test/elements/intacct/checking-accounts.js
+++ src/test/elements/intacct/checking-accounts.js
@@ -5,13 +5,14 @@ const expect = require('chakram').expect;
 
 suite.forElement('finance', 'checking-accounts', (test) => {
   test.should.supportSr();
-  test.should.supportNextPagePagination(1,false);
+  test.should.supportNextPagePagination(2, false);
   test
-     .withOptions({ qs: { where: `BANKACCOUNTTYPE >= 'checking'` } })
-     .withName('should support Ceql BANKACCOUNTTYPE search')
-     .withValidation(r => {
-       expect(r).to.statusCode(200);
-       const validValues = r.body.filter(obj => obj.BANKACCOUNTTYPE === 'checking');
-       expect(validValues.length).to.equal(r.body.length);
-     })
-     .should.return200OnGet();});
+    .withOptions({ qs: { where: `WHENMODIFIED ='08/13/2016 05:26:37'` } })
+    .withName('should support Ceql WHENMODIFIED search')
+    .withValidation(r => {
+      expect(r).to.statusCode(200);
+      const validValues = r.body.filter(obj => obj.WHENMODIFIED = '08/13/2016 05:26:37');
+      expect(validValues.length).to.equal(r.body.length);
+    })
+    .should.return200OnGet();
+});
