f5a5bda0732df7beccd20103053938730010b380
seshuede <seshuede@gmail.com>
2018-01-22 18:20:37 -0600

    Updated Test cases

--- src/test/elements/launchbiqa/folders.js
+++ src/test/elements/launchbiqa/folders.js
@@ -1,46 +1,49 @@
 'use strict';
 
 const suite = require('core/suite');
+const cloud = require('core/cloud');
 const expect = require('chakram').expect;
 
 suite.forElement('general', 'folders', (test) => {
 
   let folderId, reportId;
 
-  test.withApi(test.api)
-    .withName(`List all Root Folders /folders`)
+  test.withApi(`/hubs/general/rootFolder`)
+    .withName(`List all Root Folders /rootFolder`)
     .withValidation(r => {
       folderId = r.body[0].id;
       expect(r.body.filter(obj => obj.id !== "")).to.not.be.empty;
     })
     .should.return200OnGet();
 
-  test.withApi(test.api)
-    .withName(`List all Sub Folders /folders/:id`)
-    .withOptions({ qs: { where: "id = `${folderId}`" } })
-    .withValidation(r => {
-      expect(r.body.filter(obj => obj.id !== "")).to.not.be.empty;
-    })
-    .should.return200OnGet();
 
-  test.withApi(`/hubs/general/folders/reports`)
-    .withName(`List all reports from Root Folders /reports`)
-    .withValidation(r => {
+
+  it('should allow GET /folders/:id/contents', () => {
+    return cloud.get(`${test.api}/${folderId}/contents`)
+      .then(r => {
+        folderId = r.body[0].id;
+        expect(r.body.filter(obj => obj.id !== "")).to.not.be.empty;
+      });
+  });
+
+
+
+  it('should allow GET /folders/:id/reports', () => {
+    return cloud.get(`${test.api}/${folderId}/reports`)
+      .then(r => {
         reportId = r.body[0].id;
         expect(r.body.filter(obj => obj.id !== "")).to.not.be.empty;
-    })
+      });
+  });
+
+  test.withApi(`/hubs/general/reports/${reportId}`)
+    .withName(`Retrieve specific Report`)
+    .withValidation(r => expect(r.body.id !== ""))
     .should.return200OnGet();
 
-  test.withApi(`/hubs/general/folders/reports`)
-    .withName(`List all reports from specific Folders /folders/:id`)
-    .withOptions({ qs: { where: "id = `${folderId}`" } })
-    .withValidation(r => {
-      expect(r.body.filter(obj => obj.link !== "")).to.not.be.empty;
-    })
-    .should.return200OnGet();
 
-  test.withApi(`/hubs/general/ping-lbi`)
-    .withName(`Retrieve launchbi-status`)
+  test.withApi(`/hubs/general/ping`)
+    .withName(`Check system health`)
     .withValidation(r => expect(r.body['launchbi-status'] !== ""))
     .should.return200OnGet();
 
@@ -49,11 +52,6 @@ suite.forElement('general', 'folders', (test) => {
     .withName(`Retrieve system-info`)
     .withValidation(r => expect(r.body.server !== ""))
     .should.return200OnGet();
-
-    test.withApi(`/hubs/general/reports/${reportId}`)
-      .withName(`Retrieve specific Report`)
-      .withValidation(r => expect(r.body.id !== ""))
-      .should.return200OnGet();
 
 
   test.withApi(`/hubs/general/native`)
