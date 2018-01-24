4b481946e7ae8cfa44c1795479202e558ac477ca
seshuede <seshuede@gmail.com>
2018-01-24 01:27:49 -0600

    Test cases for Google Drive search, folders/contents and folders/:id/contents with orderBy functionality

--- src/test/elements/googledrive/folders.js
+++ src/test/elements/googledrive/folders.js
@@ -16,7 +16,7 @@ suite.forElement('documents', 'folders', { payload: payload }, (test) => {
   let jpgFile = __dirname + '/assets/Penguins.jpg';
   let pngFile = __dirname + '/assets/Dice.png';
   let textFile = __dirname + '/assets/textFile.txt';
-  let jpgFileBody, pngFileBody, textFileBody;
+  let jpgFileBody, pngFileBody, textFileBody, date1, date2;;
 
   before(() =>
     cloud.withOptions({ qs: { path: `/${directoryPath}/Penguins.jpg`, overwrite: 'true' } }).postFile(`/hubs/documents/files`, jpgFile)
@@ -88,6 +88,26 @@ suite.forElement('documents', 'folders', { payload: payload }, (test) => {
     return cloud.withOptions({ qs: { path: `/${directoryPath}`, where: "mimeType='text/plain'" } }).get(`${test.api}/contents`)
       .then(r => expect(r.body.length).to.equal(r.body.filter(obj => obj.properties.mimeType === 'text/plain').length));
   });
+
+  test.withApi(`/folders/contents`)
+    .withName(`should allow GET for /folders/contents with use of the orderBy modifiedDate parameter`)
+    .withOptions({ qs: { path: `/`, pageSize: 5, page: 1, orderBy: `modifiedDate asc` } })
+    .withValidation(r => {
+      date1 = new Date(r.body[0].createdDate).getTime();
+      date2 = new Date(r.body[1].createdDate).getTime();
+      expect(date1 <= date2).to.be.true;
+    })
+    .should.return200OnGet();
+
+    test.withApi(`${test.api}/root/contents`)
+        .withName(`should allow GET for /folders/:id/contents with use of the orderBy createdDate parameter`)
+        .withOptions({ qs: {  pageSize: 5, page: 1, orderBy: `createdDate desc` } })
+        .withValidation(r => {
+          date1 = new Date(r.body[0].createdDate).getTime();
+          date2 = new Date(r.body[1].createdDate).getTime();
+          expect(date1 >= date2).to.be.true;
+        })
+        .should.return200OnGet();
 
   //Skipping as soba PR is on hold
   it.skip('should not return the path for GET /folders/contents?includePath=false', () => {
