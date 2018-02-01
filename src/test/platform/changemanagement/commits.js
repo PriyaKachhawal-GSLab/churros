5f7c0217375bae8aaeb4024ea2adc9e1c086519c
wyselm <laura@cloud-elements.com>
2018-02-01 12:09:05 -0700

    add tests for commit APIs

---
+++ src/test/platform/changemanagement/commits.js
@@ -0,0 +1,54 @@
+'use strict';
+
+const suite = require('core/suite');
+const cloud = require('core/cloud');
+const chakram = require('chakram');
+const expect = chakram.expect;
+const R = require('ramda');
+
+const genCommit = (type, id, m) => {
+    return {
+        entityType: type,
+        entityId: id,
+        message: m
+    };
+};
+
+suite.forPlatform('change-management/commits', {payload: genCommit('elementObject', 1, 'crud test')}, test => {
+
+  test.should.supportCrud();
+
+  it('should support searching by a date range and user', () => {
+    const validator = (r, num) => {
+      expect(r).to.have.statusCode(200);
+      expect(r.body.length).to.equal(num);
+    };
+
+    let commits = [];
+    let deletes = [];
+    let startDate, endDate;
+
+    return cloud.post('/change-management/commits', genCommit('elementObject', 2, 'first'))
+        .then(r => {
+            startDate = r.body.createdDate;
+            commits.push(r.body);
+        })
+        .then(() => cloud.post('/change-management/commits', genCommit('elementObject', 2, 'second')))
+        .then(r => commits.push(r.body))
+        .then(() => cloud.post('/change-management/commits', genCommit('elementObject', 3, 'a diff model')))
+        .then(r => commits.push(r.body))
+        .then(() => cloud.post('/change-management/commits', genCommit('element', 1, 'oh snap, thats not a model commit')))
+        .then(r => {
+            endDate = r.body.createdDate;
+            commits.push(r.body);
+        })
+        .then(() => cloud.post('/change-management/commits', genCommit('elementObject', 2, 'another but after the end time')))
+        .then(r => commits.push(r.body))
+        .then(() => cloud.get(`/change-management/commits?entityType=elementObject&entityId=2&to=${endDate}&from=${startDate}`, r => validator(r, 2)))
+        .then(() => cloud.get(`/change-management/commits?entityType=elementObject&entityId=2&from=${startDate}`, r => validator(r, 3)))
+        .then(() => cloud.get(`/change-management/commits?entityType=element&entityId=1&from=${startDate}`, r => validator(r, 1)))
+        .then(() => commits.forEach(c => deletes.push(cloud.delete(`/change-management/commits/${c.id}`))))
+        .then(() => chakram.all(deletes));
+  });
+
+});
