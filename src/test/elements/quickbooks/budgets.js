c6eee45ab90fda9668d30c653edb52ad690a3203
seshuede <seshuede@gmail.com>
2018-02-05 13:34:31 -0600

    Added where

--- src/test/elements/quickbooks/budgets.js
+++ src/test/elements/quickbooks/budgets.js
@@ -1,9 +1,13 @@
 'use strict';
 
 const suite = require('core/suite');
-
+const expect = require('chakram').expect;
 suite.forElement('finance', 'budgets', (test) => {
   test.should.supportSr();
   test.should.supportPagination();
-  test.should.supportCeqlSearch('active');
+  test.withApi(test.api)
+    .withOptions({ qs: { where: "active='true'" } })
+    .withValidation(r => expect(r.body.filter(obj => obj.active === true)).to.not.be.empty)
+    .withName('should allow GET with option active')
+    .should.return200OnGet();
 });
