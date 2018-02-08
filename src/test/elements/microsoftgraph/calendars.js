f3ac301fc1c8a7e2f06c816afc7227af46234167
Jamie Thorson <jamie@cloud-elements.com>
2018-02-08 13:23:29 -0700

    added semicolon

--- src/test/elements/microsoftgraph/calendars.js
+++ src/test/elements/microsoftgraph/calendars.js
@@ -8,11 +8,12 @@ const cloud = require('core/cloud');
 suite.forElement('general', 'calendars', { payload: payload }, (test) => {
   test.should.supportCrud();
   test.should.supportPagination();
-// the CEQL values are hard coded in do to a problem with microsoft api's that don't allow you to create a calendar, query, and delete it
+  // the CEQL values are hard coded in do to a problem with microsoft api's that don't allow you to create a calendar, query, and delete it
   it('should allow CEQL', () => {
     return cloud.get(`${test.api}?where=name%3D'sample%20for%20display7'`)
-    .then(r => function(){(r.body[0].name).should.equal("sample for display7")});
-  }
-);
+      .then(r => function() {
+        (r.body[0].name).should.equal("sample for display7");
+      });
+  });
 
 });
