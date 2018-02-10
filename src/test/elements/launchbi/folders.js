250cead5cf990fb44a20b16c63785c7850a39b08
seshuede <seshuede@gmail.com>
2018-02-10 00:22:00 -0600

    work on review comments

--- src/test/elements/launchbi/folders.js
+++ src/test/elements/launchbi/folders.js
@@ -8,13 +8,11 @@ suite.forElement('analytics', 'folders', (test) => {
 
   let folderId, reportId;
 
-
-
   it('should allow GET /folders', () => {
     return cloud.get(test.api)
       .then(r => {
-        folderId = r.body[0].id;
         expect(r.body.filter(obj => obj.id !== "")).to.not.be.empty;
+        folderId = r.body[0].id;
       })
       .then(() => cloud.withOptions({ qs: { id: folderId } }).get(test.api))
       .then(r => folderId = r.body[0].id);
@@ -23,33 +21,33 @@ suite.forElement('analytics', 'folders', (test) => {
   it('should allow GET /folders/:id/reports', () => {
     return cloud.get(`/hubs/analytics/folders/${folderId}/reports`)
       .then(r => {
-        reportId = r.body[0].id;
         expect(r.body.filter(obj => obj.id !== "")).to.not.be.empty;
+        reportId = r.body[0].id;
       });
   });
 
   test.withApi(`/hubs/analytics/reports/${reportId}`)
     .withName(`Retrieve specific Report`)
-    .withValidation(r => expect(r.body.id !== ""))
+    .withValidation(r => expect(r.body).to.not.be.empty)
     .should.return200OnGet();
 
 
   test.withApi(`/hubs/analytics/ping`)
     .withName(`Check system health`)
-    .withValidation(r => expect(r.body['launchbi-status'] !== ""))
+    .withValidation(r => expect(r.body).to.not.be.empty)
     .should.return200OnGet();
 
 
   test.withApi(`/hubs/analytics/info`)
     .withName(`Retrieve system-info`)
-    .withValidation(r => expect(r.body.server !== ""))
+    .withValidation(r => expect(r.body).to.not.be.empty)
     .should.return200OnGet();
 
 
   test.withApi(`/hubs/analytics/native`)
     .withName(`List all native`)
     .withOptions({ qs: { 'resourceName': 'schedules' } })
-    .withValidation(r => expect(r.body[0].id !== ""))
+    .withValidation(r => expect(r.body[0]).to.not.be.empty)
     .should.return200OnGet();
 
 });
