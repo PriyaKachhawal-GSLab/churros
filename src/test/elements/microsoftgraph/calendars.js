404e37025fb2238e7732dfc17380869fefbc6dc5
Jamie Thorson <jamie@cloud-elements.com>
2018-02-12 14:13:40 -0700

    another recommended change

--- src/test/elements/microsoftgraph/calendars.js
+++ src/test/elements/microsoftgraph/calendars.js
@@ -9,7 +9,6 @@ suite.forElement('general', 'calendars', { payload: payload }, (test) => {
   test.should.supportCrud();
   test.should.supportPagination();
   // the CEQL values are hard coded in do to a problem with microsoft api's that don't allow you to create a calendar, query, and delete it
-  it('should allow CEQL', () => {
     test.withName(`should support searching ${test.api} by name`)
     .withOptions({ qs: { where: `name ='sample for display7'` } })
     .withValidation((r) => {
@@ -17,6 +16,6 @@ suite.forElement('general', 'calendars', { payload: payload }, (test) => {
       const validValues = r.body.filter(obj => obj.name === 'sample for display7');
       expect(validValues.length).to.equal(r.body.length);
     }).should.return200OnGet();
-  });
+
 
 });
