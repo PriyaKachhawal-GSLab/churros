554f1f9e595dcaaa83d2a43fe6c28701eed73f0a
Taylor Clark <taylor@cloud-elements.com>
2018-02-15 10:21:39 -0600

    added tests for visits & visitors

---
+++ src/test/elements/pardot/visitors.js
@@ -0,0 +1,19 @@
+'use strict';
+
+const suite = require('core/suite');
+const cloud = require('core/cloud');
+
+suite.forElement('marketing', 'visitors', {}, (test) => {
+  let visitorId, visitId;
+  
+  test.should.supportS();
+  test.should.supportPagination();
+  it(`should support SR for ${test.api}/:id/visits`, () => {
+    let visitorId, visitId;
+    return cloud.get(test.api)
+    .then(r => visitorId = r.body[0].id)
+    .then(() => cloud.get(`${test.api}/${visitorId}/visits`))
+    .then(r => visitId = r.body[0].id)
+    .then(() => cloud.get(`${test.api}/${visitorId}/visits/${visitId}`))
+  })
+});
