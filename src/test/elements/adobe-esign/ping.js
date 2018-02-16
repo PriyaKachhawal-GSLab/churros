4ebb92a0cbc04fe30826672c2edf42a940f320d4
seshuede <seshuede@gmail.com>
2018-02-16 15:42:55 -0600

    work on review comments

--- src/test/elements/adobe-esign/ping.js
+++ src/test/elements/adobe-esign/ping.js
@@ -3,9 +3,13 @@
 const suite = require('core/suite');
 const expect = require('chakram').expect;
 
-suite.forElement('esignature', 'ping', null, (test) => {
+suite.forElement('esignature', 'ping', (test) => {
   test.withApi(test.api)
     .withName(`should allow GET /ping for system health`)
-    .withValidation(r => expect(r.body.endpoint).to.equal('adobe-esign'))
+    .withValidation(r => {
+      expect(r).to.have.statusCode(200);
+      expect(r.body.endpoint).to.equal('adobe-esign');
+      expect(r.body.valid).to.equal(true);
+    })
     .should.return200OnGet();
 });
