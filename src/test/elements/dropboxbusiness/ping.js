76c651d448c1a9b40ecdd620a217b55ef01da68d
seshuede <seshuede@gmail.com>
2018-02-16 15:29:01 -0600

    work on review comments

--- src/test/elements/dropboxbusiness/ping.js
+++ src/test/elements/dropboxbusiness/ping.js
@@ -3,9 +3,13 @@
 const suite = require('core/suite');
 const expect = require('chakram').expect;
 
-suite.forElement('documents', 'ping', null, (test) => {
+suite.forElement('documents', 'ping', (test) => {
   test.withApi(test.api)
     .withName(`should allow GET /ping for system health`)
-    .withValidation(r => expect(r.body.endpoint).to.equal('dropboxbusiness'))
+    .withValidation(r => {
+      expect(r).to.have.statusCode(200);
+      expect(r.body.endpoint).to.equal('dropboxbusiness');
+      expect(r.body.valid).to.equal(true);
+    })
     .should.return200OnGet();
 });
