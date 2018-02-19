2e1136e16700e9a852e20e8c10bbfc02763acf0b
Mithila Garud <mithila.garud@gslab.com>
2018-02-19 17:15:40 +0530

    Resolved merge conflicts

--- src/test/elements/intacct/bills-payments.js
+++ src/test/elements/intacct/bills-payments.js
@@ -7,11 +7,11 @@ const expect = require('chakram').expect;
 suite.forElement('finance', 'bills-payments', (test) => {
   test.should.supportSr();
   test
-    .withOptions({ qs: { where: `STATE = 'C'` } })
-    .withName('should support Ceql search')
+    .withOptions({ qs: { where: `WHENMODIFIED >= '06/23/2013 14:25:17'` } })
+    .withName('should support Ceql date search')
     .withValidation(r => {
       expect(r).to.statusCode(200);
-      const validValues = r.body.filter(obj => obj.STATE = 'C');
+      const validValues = r.body.filter(obj => new Date(obj.WHENMODIFIED).getTime() >= 1372015517000); //06/23/2013 14:25:17 is equivalent to 1372015517000
       expect(validValues.length).to.equal(r.body.length);
     })
     .should.return200OnGet();
