93558909216cb07755d2443675d4b345790bc70a
Garima Agarwal <garima.agarwal@gslab.com>
2018-02-23 13:33:59 +0530

    Removing incorrect file from PR

--- src/test/elements/eloqua/accounts.js
+++ src/test/elements/eloqua/accounts.js
@@ -2,8 +2,6 @@
 
 const suite = require('core/suite');
 const payload = require('./assets/accounts');
-const chakram = require('chakram');
-const expect = chakram.expect;
 
 suite.forElement('marketing', 'accounts', { payload: payload }, (test) => {
   const opts = {
@@ -14,12 +12,4 @@ suite.forElement('marketing', 'accounts', { payload: payload }, (test) => {
     }
   };
   test.withOptions(opts).should.supportCruds();
-  test.withOptions({ qs: { page: 1, pageSize: 5 } }).should.return200OnGet();
-  test.withName(`should support searching ${test.api} by Country`)
-    .withOptions({ qs: { where: `country='India'` } })
-    .withValidation((r) => {
-      expect(r).to.have.statusCode(200);
-      const validValues = r.body.filter(obj => obj.country = 'India');
-      expect(validValues.length).to.equal(r.body.length);
-    }).should.return200OnGet();
 });
