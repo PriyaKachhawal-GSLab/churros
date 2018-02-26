0126e49363b31761234f50d10f26a93881cab540
madhuri.kadam <madhuri.kadam@gslab.com>
2018-02-26 15:08:09 +0530

    updated review comments

--- src/test/elements/dropbox/custom-fields-templates.js
+++ src/test/elements/dropbox/custom-fields-templates.js
@@ -4,22 +4,14 @@ const suite = require('core/suite');
 const cloud = require('core/cloud');
 const tools = require('core/tools');
 const temPayload = tools.requirePayload(`${__dirname}/assets/template.json`);
+const updateTemPayload = tools.requirePayload(`${__dirname}/assets/UpdateTemplate.json`);
 
 suite.forElement('documents', 'custom-fields-templates', (test) => {
  let tempKey;
   it('should support CRUS for /custom-fields-templates/templates', () => {
-    let updatePayload = {
-      "add_fields": [
-        {
-            "name": "Security Policy",
-            "description": "This is the security policy of the file or folder described.\nPolicies can be Confidential, Public or Internal.",
-            "type": "string"
-        }
-      ]
-    };
     return cloud.post(`${test.api}`, temPayload)
       .then(r => tempKey = r.body.template_id)
-      .then(r => cloud.put(`${test.api}/${tempKey}`, updatePayload))
+      .then(r => cloud.put(`${test.api}/${tempKey}`, updateTemPayload))
       .then(r => cloud.get(`${test.api}/${tempKey}`))
       .then(r => cloud.get(`${test.api}`))
       .then(r => cloud.delete(`${test.api}/${tempKey}`));
--- src/test/elements/dropbox/files.js
+++ src/test/elements/dropbox/files.js
@@ -3,19 +3,33 @@
 const cloud = require('core/cloud');
 const suite = require('core/suite');
 const tools = require('core/tools');
-const temPayload = tools.requirePayload(`${__dirname}/assets/template.json`);
 const payload=tools.requirePayload(`${__dirname}/assets/customFields.json`);
+const updatePayload=tools.requirePayload(`${__dirname}/assets/UpdateCustomFields.json`);
+const temPayload = tools.requirePayload(`${__dirname}/assets/template.json`);
 
 suite.forElement('documents','files',(test) => {
 
   let jpgFile = __dirname + '/assets/brady.jpg';
+  let tempKey;
+  let customBody;
   var jpgFileBody,revisionId;
   let query = { path: `/brady-${tools.randomStr('abcdefghijklmnopqrstuvwxyz1234567890', 10)}.jpg` };
 
-  before(() => cloud.withOptions({ qs : query }).postFile(test.api, jpgFile)
-  .then(r => jpgFileBody = r.body));
 
-  after(() => cloud.delete(`${test.api}/${jpgFileBody.id}`));
+
+  before(() => cloud.withOptions({ qs : query }).postFile(test.api, jpgFile)
+  .then(r => jpgFileBody = r.body)
+  .then(r => cloud.post('/hubs/documents/custom-fields-templates', temPayload))
+      .then(r => {
+        customBody= r.body;
+        tempKey = r.body.template_id;
+        updatePayload.add_or_update_fields[0].name = temPayload.fields[0].name;
+        payload.fields[0].name = temPayload.fields[0].name;
+      }));
+
+  after(() => cloud.delete(`${test.api}/${jpgFileBody.id}`)
+  .then(r => cloud.delete(`/hubs/documents/custom-fields-templates/${tempKey}`))
+);
 
   it('it should allow RS for documents/files/:id/revisions', () => {
       return cloud.get(`${test.api}/${jpgFileBody.id}/revisions`)
@@ -30,15 +44,7 @@ suite.forElement('documents','files',(test) => {
   });
 
   it(' it should allow RS for /files/{id}/custom-fields-templates/{templateKeyId}/custom-fields', () => {
-    let tempKey;
-    let updatePayload = {
-      "add_or_update_fields": [
-                      {
-                          "name": "Security Policy",
-                          "value": "Confidential"
-                      }
-        ]
-    };
+
     return cloud.post('/hubs/documents/custom-fields-templates', temPayload)
         .then(r => {
           tempKey = r.body.template_id;
@@ -47,9 +53,7 @@ suite.forElement('documents','files',(test) => {
         })
         .then(r => cloud.post(`/files/${jpgFileBody.refId}/custom-fields-templates/${tempKey}/custom-fields`, payload))
         .then(r => cloud.put(`/hubs/documents/files/${jpgFileBody.refId}/custom-fields-templates/${tempKey}/custom-fields`, updatePayload))
-        .then(r => cloud.delete(`/hubs/documents/files/${jpgFileBody.refId}/custom-fields-templates/${tempKey}/custom-fields`))
-        .then(r => cloud.delete(`/hubs/documents/custom-fields-templates/${tempKey}`));
-
+        .then(r => cloud.delete(`/hubs/documents/files/${jpgFileBody.refId}/custom-fields-templates/${tempKey}/custom-fields`));
   });
 
 
