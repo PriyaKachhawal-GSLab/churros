fd967413a3c3f972843d9bdf4c0594d9bafa6ace
Atul Barve <atul@cloud-elements.com>
2018-02-26 16:10:30 -0600

    Fixed Review Comments

--- src/test/elements/quickbooksonprem/tax-rates.js
+++ src/test/elements/quickbooksonprem/tax-rates.js
@@ -12,11 +12,11 @@ suite.forElement('finance', 'tax-rates', { payload: payload }, (test) => {
     return cloud.post(test.api, payload)
       .then(r => id = r.body.ListID)
       .then(r => cloud.get(test.api))
-      .then(r => cloud.withOptions({ qs: { page: 1, pageSize: 1 } }).get(test.api))
       .then(r => cloud.withOptions({ qs: { where: `ListID='${id}'` } }).get(test.api))
       .then(r => cloud.get(`${test.api}/${id}`))
       .then(r => updatePayload.EditSequence = r.body.EditSequence)
       .then(r => cloud.patch(`${test.api}/${id}`, updatePayload))
       .then(r => cloud.delete(`${test.api}/${id}`));
   });
+  test.should.supportNextPagePagination(1);
 });
