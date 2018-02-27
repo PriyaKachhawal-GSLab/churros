53a09b5b810f81ed076b4a879d333dac96b02a23
Taylor Clark <taylor@cloud-elements.com>
2018-02-26 20:10:13 -0600

    added `break` to case statement ...
    ...to satisfy coveralls

--- src/core/oauth.js
+++ src/core/oauth.js
@@ -700,6 +700,7 @@ const manipulateDom = (element, browser, r, username, password, config) => {
         .then(() => browser.sleep(2000))
         .then(() => browser.getCurrentUrl());
       }
+      break;
     default:
       throw 'No OAuth function found for element ' + element + '.  Please implement function in core/oauth so ' + element + ' can be provisioned';
   }
