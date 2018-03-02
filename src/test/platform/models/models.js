648b0c78da396fd67dc0acb051e0b13b5227217e
Tyler Toth <tyler@cloud-elements.com>
2018-03-02 13:11:57 -0600

    Model tests per latest changes

--- src/test/platform/models/models.js
+++ src/test/platform/models/models.js
@@ -10,6 +10,7 @@ const listSchema = require('./assets/listPayload.schema.json');
 const defaultCsvHeaders = ['sampleValue','name','type','format','subFormat','displayName','description','request','response','defaultValue','max','min','precision','nullable','searchOperators','searchNames','mask','enums','conditionalDescription','reference','hidden','primaryKey','readOnly','required','vendorReferenceName','vendorName','customFields','searchable','whereFields','searchableJoins'];
 
 suite.forPlatform('models', {}, (test) => {
+  let csvBody;
   it('should generate model schema from payload', () => {
     return cloud.post('/models/invoices/schema', payload)
       .then(r => expect(r.body).to.deep.equal(schema));
@@ -26,10 +27,23 @@ suite.forPlatform('models', {}, (test) => {
         expect(r.response.headers['content-disposition']).to.contain('attachment;');
         expect(r.response.headers['content-type']).to.equal('text/csv');
         expect(r.body).to.not.be.empty;
+        csvBody = r.body;
         let bodyHeaders = r.body.split('\n')[0].split(',');
         expect(bodyHeaders).to.include.members(defaultCsvHeaders);
       });
   });
+
+  it('should generate csv from schema', () => {
+    return cloud.withOptions({ headers: { accept: 'text/csv' }, qs: { fromSchema: true }}).post('/models/invoices/csv', schema)
+      .then(r => {
+        expect(r.response.headers['content-disposition']).to.contain('attachment;');
+        expect(r.response.headers['content-type']).to.equal('text/csv');
+        expect(r.body).to.not.be.empty;
+        let bodyHeaders = r.body.split('\n')[0].split(',');
+        expect(bodyHeaders).to.include.members(defaultCsvHeaders);
+        expect(r.body).to.equal(csvBody);
+      });
+  })
 
   it('should generate model schema from csv payload', () => {
     return cloud.postFile('/models/invoices/schema/csv', `${__dirname}/assets/csvPayload.csv`)
