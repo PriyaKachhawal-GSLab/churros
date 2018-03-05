5595e267a8d474b3cb9666f1b7e438a34f5c91a8
Isha B Deshpande <GSC-30169@GSLAB.COM>
2018-03-05 18:41:59 +0530

    added the validatrion check code

--- src/test/elements/servicemax/installed-products.js
+++ src/test/elements/servicemax/installed-products.js
@@ -4,6 +4,8 @@ const suite = require('core/suite');
 const payload = require('./assets/installed-products');
 const tools = require('core/tools');
 const cloud = require('core/cloud');
+const chakram = require('chakram');
+const expect = chakram.expect;
 const build = (overrides) => Object.assign({}, payload, overrides);
 const productsPayload = build({ Name: tools.random() });
 
@@ -18,10 +20,16 @@ suite.forElement('fsa', 'installed-products', { payload: productsPayload }, (tes
   test.withOptions(options).should.supportCruds();
   test.should.supportPagination();
   it('should test "where" search for installed-products', () => {
-      let id;
-      return cloud.post(test.api, productsPayload)
-        .then(r => id = r.body.id)
-        .then(r => cloud.get(test.api), { qs: {  where:  `Id =\'${id}\'` } })
-        .then(r => cloud.delete(`${test.api}/${id}`));
-    });
+    let id;
+    return cloud.post(test.api, productsPayload)
+      .then(r => {
+        id = r.body.id;
+        const myOptions = {qs: { where:  `Id =\'${id}\'`}};
+        return cloud.withOptions(myOptions).get(test.api, (r) => {
+          expect(r).to.have.statusCode(200);
+          expect(r.body.filter(obj => obj.Id === id).length).to.equal(r.body.length);
+        });
+       })
+      .then(r => cloud.delete(`${test.api}/${id}`));
+  });
 });
--- src/test/elements/servicemax/work-orders.js
+++ src/test/elements/servicemax/work-orders.js
@@ -2,6 +2,8 @@
 
 const suite = require('core/suite');
 const cloud = require('core/cloud');
+const chakram = require('chakram');
+const expect = chakram.expect;
 const payload = require('./assets/work-orders');
 
 suite.forElement('fsa', 'work-orders', { payload: payload }, (test) => {
@@ -9,10 +11,16 @@ suite.forElement('fsa', 'work-orders', { payload: payload }, (test) => {
   test.should.supportCruds();
   test.should.supportPagination();
   it('should test "where" search for work-orders', () => {
-      let id;
-      return cloud.post(test.api, payload)
-        .then(r => id = r.body.id)
-        .then(r => cloud.get(test.api), { qs: {  where:  `Id =\'${id}\'` } })
-        .then(r => cloud.delete(`${test.api}/${id}`));
-    });
+    let id;
+    return cloud.post(test.api, payload)
+      .then(r => {
+        id = r.body.id;
+        const myOptions = {qs: { where:  `Id =\'${id}\'`}};
+        return cloud.withOptions(myOptions).get(test.api, (r) => {
+          expect(r).to.have.statusCode(200);
+          expect(r.body.filter(obj => obj.Id === id).length).to.equal(r.body.length);
+        });
+      })
+      .then(r => cloud.delete(`${test.api}/${id}`));
+  });
 });
