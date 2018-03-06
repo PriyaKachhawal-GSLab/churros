8f5033c1d430ab2324c36697c161255f875ba8e8
Mayur Dhande <mayur.dhande@gslab.com>
2018-03-06 15:28:24 +0530

    beautified

--- src/test/elements/netsuiteerpv2/budgets.js
+++ src/test/elements/netsuiteerpv2/budgets.js
@@ -4,18 +4,14 @@ const suite = require('core/suite');
 const expect = require('chakram').expect;
 const cloud = require('core/cloud');
 
-suite.forElement('erp', 'budgets', {skip: true}, (test) => {
+suite.forElement('erp', 'budgets', { skip: true }, (test) => {
 
   it('should allow SRUD for hubs/erp/budgets and CEQL search', () => {
-    let budgetId;
-	let payload = {};
+    let budgetId, payload = {};
     return cloud.get(`${test.api}`)
       .then(r => budgetId = r.body[0].id)
       .then(r => cloud.withOptions({ qs: { where: `id=${budgetId}` } }).get(`${test.api}`))
-      .then(r => {
-      expect(r).to.statusCode(200);
-      expect(r.body.length).to.be.equal(1);
-    })
+      .then(r => expect(r.body.length).to.be.equal(1))
       .then(r => cloud.get(`${test.api}/${budgetId}`))
       .then(r => cloud.patch(`${test.api}/${budgetId}`, payload))
       .then(r => cloud.delete(`${test.api}/${budgetId}`));
--- src/test/elements/netsuiteerpv2/currencies.js
+++ src/test/elements/netsuiteerpv2/currencies.js
@@ -3,9 +3,9 @@
 const suite = require('core/suite');
 const tools = require('core/tools');
 const payload = tools.requirePayload(`${__dirname}/assets/currencies.json`);
+payload.symbol = tools.randomStr("AMNBGHJOPEIOU", 3);
 
 suite.forElement('erp', 'currencies', { payload: payload }, (test) => {
-payload.symbol = tools.randomStr("AMNBGHJOPEIOU", 3);
   test.should.supportCruds();
   test.withOptions({ qs: { page: 1, pageSize: 5 } }).should.supportPagination();
 });
