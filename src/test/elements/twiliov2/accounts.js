196ffdb0910ccb46383321e70b00f36e5983b27e
AbhayPore-GSLab <34128818+AbhayPore-GSLab@users.noreply.github.com>
2018-03-07 07:50:13 +0530

    incorporated review comments

--- src/test/elements/twiliov2/accounts.js
+++ src/test/elements/twiliov2/accounts.js
@@ -1,18 +1,17 @@
 'use strict';
 
 const suite = require('core/suite');
-const payload = require('./assets/addresses');
-var chakram = require('chakram'),
-    expect = chakram.expect;
+const expect = require('chakram').expect;
 
-suite.forElement('messaging', 'accounts', {payload: payload}, (test) => {
+suite.forElement('messaging', 'accounts', (test) => {
   test.should.supportSr();
+  test.should.supportPagination();
   test
      .withOptions({ qs: { where: `FriendlyName = 'Jack' AND Status='suspended'` } })
      .withName('should support Ceql FriendlyName search')
      .withValidation(r => {
        expect(r).to.statusCode(200);
-       const validValues = r.body.filter(obj => obj.FriendlyName = 'Jack');
+       const validValues = r.body.filter(obj => obj.friendly_name = 'Jack' && obj.status === 'suspended');
        expect(validValues.length).to.equal(r.body.length);
      })
      .should.return200OnGet();
--- src/test/elements/twiliov2/addresses.js
+++ src/test/elements/twiliov2/addresses.js
@@ -7,12 +7,13 @@ const payload = require('./assets/addresses');
 
 suite.forElement('messaging', 'addresses', {payload: payload}, (test) => {
   test.should.supportCruds();
+  test.should.supportPagination();
   test
      .withOptions({ qs: { where: `FriendlyName = 'Jack' ` } })
      .withName('should support Ceql FriendlyName search')
      .withValidation(r => {
        expect(r).to.statusCode(200);
-       const validValues = r.body.filter(obj => obj.FriendlyName = 'Jack');
+       const validValues = r.body.filter(obj => obj.friendly_name = 'Jack');
        expect(validValues.length).to.equal(r.body.length);
      })
      .should.return200OnGet();
