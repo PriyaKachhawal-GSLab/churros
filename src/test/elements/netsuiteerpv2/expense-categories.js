49a1b79b017839beebd3367ffcce991163f980a7
Atul Barve <atul@cloud-elements.com>
2018-03-08 16:00:23 -0600

    Fixed Churros And added Churros for Expense Categories, Phone Calls,

--- src/test/elements/netsuiteerpv2/expense-categories.js
+++ src/test/elements/netsuiteerpv2/expense-categories.js
@@ -2,14 +2,14 @@
 
 const suite = require('core/suite');
 const cloud = require('core/cloud');
-const expenseReportsPayload = require('./assets/expense-reports');
+const expenseCategoryPayload = require('./assets/expense-categories');
 
 
-suite.forElement('finance', 'expense-reports', (test) => {
+suite.forElement('erp', 'expense-categories', (test) => {
   test.withOptions({ qs: { page: 1, pageSize: 5 } }).should.supportPagination();
-  it('should allow CRUDS /hubs/finance/expense-reports', () => {
+  it('should allow CRUDS /hubs/erp/expense-categories', () => {
     let internalId;
-    return cloud.post(test.api, expenseReportsPayload)
+    return cloud.post(test.api, expenseCategoryPayload)
       .then(r => internalId = r.body.id)
       .then(r => cloud.get(test.api))
       .then(r => cloud.get(`${test.api}/${internalId}`))
