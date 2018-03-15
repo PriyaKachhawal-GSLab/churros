d0244a4dc42efcdee8ed5ae05ecb93e14d595c06
AfrinChakure-GSLab <afrin.chakure@gslab.com>
2018-03-15 22:36:17 +0530

    Incorporated review comments

--- src/test/elements/autotaskhelpdesk/contracts.js
+++ src/test/elements/autotaskhelpdesk/contracts.js
@@ -2,9 +2,18 @@
 
 const suite = require('core/suite');
 const contractPayload = require('./assets/contracts');
+const expect = require('chakram').expect;
 
-suite.forElement('helpdesk', 'contracts', { payload: contractPayload }, (test) => {
+suite.forElement('helpdesk', 'contracts', { payload: contractPayload, skip: true }, (test) => {	
 	 test.should.supportCrus();
-	 test.withOptions({ qs: { where: 'contactName=\'Wilson, Carl\'' } }).should.return200OnGet();
-        test.should.supportPagination('id');
+	 test.should.supportPagination('id');
+	 test
+	 .withOptions({ qs: { where: 'contactName=\'Wilson, Carl\'' } })
+	 .withName('should support search by contactName')
+	 .withValidation(r => {
+		 expect(r).to.statusCode(200);
+		 const validValues = r.body.filter(obj => obj.contactName = `Wilson, Carl`);
+		 expect(validValues.length).to.equal(r.body.length);
+    })
+	.should.return200OnGet();
  });
