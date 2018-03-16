66b900ba813c23aacf7a6e4854b3e64877d63e9f
AbhayPore-GSLab <34128818+AbhayPore-GSLab@users.noreply.github.com>
2018-03-16 14:36:33 +0530

    Greenhouse: added new element

---
+++ src/test/elements/greenhouse/candidates.js
@@ -0,0 +1,43 @@
+'use strict';
+const expect = require('chakram').expect;
+const suite = require('core/suite');
+const payload = require('./assets/candidates');
+const attachmentPayload=require('./assets/attachment')
+const notesPayload=require('./assets/notes')
+const cloud = require('core/cloud');
+const opts = { formData: { body: JSON.stringify(attachmentPayload) } };
+const queryString={ qs: { 'User Id': 461299 } }
+
+const headers = { 'Content-Type': 'multipart/form-data' };
+
+suite.forElement('general', 'candidates', { payload: payload, }, (test) => {
+	
+   let candidateId, resumeFile = __dirname + '/assets/resume.txt';;
+   before(() => cloud.post(test.api, payload)
+    .then(r => candidateId = r.body.id));
+   after(() => cloud.withOptions(queryString).delete(`${test.api}/${candidateId}`));
+
+  it('should test POST of /attachments', () => {
+	return cloud.withOptions(opts).postFile(`${test.api}/${candidateId}/attachments`, resumeFile);
+  });
+  
+  test.withOptions(queryString).should.supportCruds();
+  test.should.supportPagination(1);
+  test.withApi(test.api)
+    .withOptions({ qs: { where: "candidate_ids=47436388 and job_id=418899" } })
+    .withValidation(r => expect(r.body.filter(obj => obj.id >= "47436388")).to.not.be.empty)
+    .withName('should allow GET with option candidateId and job_id')
+    .should.return200OnGet();
+  
+  it('should test CR of /notes', () => {
+	return cloud.post(`${test.api}/${candidateId}/notes`,notesPayload)
+	.then(r =>cloud.get(`${test.api}/${candidateId}/notes`))
+	.then(r=>{
+		expect(r.body.length).to.not.be.empty
+	});
+  });
+  
+}); 
+
+  
+  
