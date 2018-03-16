603f33ee07811f175d90c6b2e62688724414e159
Mayur Dhande <mayur.dhande@gslab.com>
2018-03-16 14:06:10 +0530

    added comments and updated file name

---
+++ src/test/elements/infusionsoftcrm/products-inventory.js
@@ -0,0 +1,17 @@
+'use strict';
+
+const suite = require('core/suite');
+const cloud = require('core/cloud');
+const payload = require('./assets/products-inventory');
+
+suite.forElement('crm', 'products', { skip: true, payload: payload }, (test) => {
+// There is no retriveAll operation neither POST operation supported by vendor. Hence productId is hardcoded in the test
+// that is specific to the account.
+  it('Should allow to GET and PATCH /products/{id}/inventory', () => {
+    let productId = 4;
+    return cloud.get(`${test.api}/${productId}/inventory`)
+      .then(r => cloud.patch(`${test.api}/${productId}/inventory`, payload))
+      .then(r => payload.Action = 'decrease')
+      .then(r => cloud.patch(`${test.api}/${productId}/inventory`, payload));
+  });
+});
