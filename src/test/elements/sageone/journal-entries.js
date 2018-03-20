52d5a5d8c8da4650aa6173bb70014d55d0179383
MithilaGarud-GSLab <mithila.garud@gslab.com>
2018-03-20 17:40:59 +0530

    Fix journal-entries test cases

--- src/test/elements/sageone/journal-entries.js
+++ src/test/elements/sageone/journal-entries.js
@@ -3,13 +3,27 @@
 const suite = require('core/suite');
 const tools = require('core/tools');
 const expect = require('chakram').expect;
+const cloud = require('core/cloud');
 const payload = require('./assets/journal-entries');
 const build = (overrides) => Object.assign({}, payload, overrides);
 const journalPayload = build({ date: Date(), reference: "re" + tools.randomInt() });
 
 suite.forElement('finance', 'journal-entries', { payload: journalPayload }, (test) => {
   let lastdate = '2017-05-11T10:26:55Z';
-  test.should.supportCrs();
+  let ledger_account_id;
+  it(`should support CRS ${test.api}`, () => {
+    cloud.get(`/hubs/finance/ledger-accounts`)
+      .then(r => {
+        if (r.body.length <= 0) {
+          return;
+        }
+        ledger_account_id = r.body[0].id;
+      });
+    payload.journal_lines[0].ledger_account_id = ledger_account_id;
+    payload.journal_lines[1].ledger_account_id = ledger_account_id;
+    test.should.supportCrs();
+  });
+
   test.should.supportPagination();
   test
     .withName(`should support searching ${test.api} by date`)
