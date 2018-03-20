7d7560412d36dcfa7c10deb705ed3344f327417c
MithilaGarud-GSLab <mithila.garud@gslab.com>
2018-03-20 17:53:45 +0530

    Fix circle CI

--- src/test/elements/sageone/invoices.js
+++ src/test/elements/sageone/invoices.js
@@ -57,7 +57,7 @@ suite.forElement('finance', 'invoices', { payload: purchaseCreditPayload }, (tes
             const validValues = r.body.filter(obj => obj.reference === `${code}`);
             expect(validValues.length).to.equal(r.body.length);
           }).should.return200OnGet();
-        return cloud.withOptions({ qs: { void_reason: `Temporary Reason` } }).delete(`${test.api}/${id}`)
+        return cloud.withOptions({ qs: { void_reason: `Temporary Reason` } }).delete(`${test.api}/${id}`);
       });
   });
 });
