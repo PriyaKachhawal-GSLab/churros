62a9d8a6e55b03fcc3ca6af92b4a3116ecd48168
MithilaGarud-GSLab <mithila.garud@gslab.com>
2018-03-20 17:45:36 +0530

    Fix other-payments test cases

--- src/test/elements/sageone/other-payments.js
+++ src/test/elements/sageone/other-payments.js
@@ -5,12 +5,40 @@ const expect = require('chakram').expect;
 const tools = require('core/tools');
 const payload = require('./assets/other-payments');
 const chakram = require('chakram');
+const cloud = require('core/cloud');
 const build = (overrides) => Object.assign({}, payload, overrides);
 const otherpaymentsPayload = build({ reference: "re" + tools.randomInt(), date: Date() });
 
 suite.forElement('finance', 'other-payments', { payload: otherpaymentsPayload }, (test) => {
   let id = "VENDOR_PAYMENT";
-  test.should.supportCruds(chakram.put);
+  let bank_account_id, contact_id, ledger_account_id;
+  it(`should support CRUDS ${test.api}`, () => {
+    cloud.get(`/hubs/finance/contacts`)
+      .then(r => {
+        if (r.body.length > 0) {
+          return;
+        }
+        contact_id = r.body[0].id;
+      });
+    cloud.get(`/hubs/finance/ledger-accounts`)
+      .then(r => {
+        if (r.body.length <= 0) {
+          return;
+        }
+        ledger_account_id = r.body[0].id;
+      });
+    cloud.get(`/hubs/finance/bank_accounts`)
+      .then(r => {
+        if (r.body.length <= 0) {
+          return;
+        }
+        bank_account_id = r.body[0].id;
+      });
+    payload.contact_id = contact_id;
+    payload.payment_lines[0].ledger_account_id = ledger_account_id;
+    payload.bank_account_id = bank_account_id;
+    test.should.supportCruds(chakram.put);
+  });
   test.should.supportPagination();
   test
     .withName(`should support searching ${test.api} by transaction_type_id`)
