b9e9812bc9079f8fa26e4b9f7f71f98ea347933c
MithilaGarud-GSLab <mithila.garud@gslab.com>
2018-03-20 17:23:27 +0530

    Fix purchase-credit-notes test cases

--- src/test/elements/sageone/purchase-credit-notes.js
+++ src/test/elements/sageone/purchase-credit-notes.js
@@ -10,22 +10,46 @@ const build = (overrides) => Object.assign({}, payload, overrides);
 const purchaseCreditPayload = build({ reference: "PCN" + tools.randomInt() });
 
 suite.forElement('finance', 'purchase-credit-notes', { payload: purchaseCreditPayload }, (test) => {
-  let code, id;
-  test.should.supportCrus(chakram.put);
+  let code, id, contact_id, ledger_account_id;
+
+
+  it(`should support GET ${test.api}`, () => {
+    cloud.get(`/hubs/finance/contacts`)
+      .then(r => {
+        if (r.body.length > 0) {
+          contact_id = r.body[0].id;
+        }
+      });
+    cloud.get(`/hubs/finance/ledger-accounts`)
+      .then(r => {
+        if (r.body.length > 0) {
+          ledger_account_id = r.body[0].id;
+        }
+      });
+    payload.contact_id = contact_id;
+    payload.credit_note_lines.ledger_account_id = ledger_account_id;
+    test.should.supportCrus(chakram.put);
+  });
+
   test.should.supportPagination();
   it(`should support GET ${test.api}`, () => {
     return cloud.get(test.api)
-      .then(r => code = r.body[0].reference)
-      .then(r => cloud.get(test.api))
-      .then(r => id = r.body[0].id)
-      .then(r => cloud.withOptions({ qs: { void_reason: `Temporary Reason` } }).delete(`${test.api}/${id}`));
+      .then(r => {
+        if (r.body.length <= 0) {
+          return;
+        }
+        code = r.body[0].reference;
+        id = r.body[0].id;
+        test
+          .withName(`should support searching ${test.api} by reference`)
+          .withOptions({ qs: { where: `search ='${code}'` } })
+          .withValidation((r) => {
+            expect(r).to.have.statusCode(200);
+            const validValues = r.body.filter(obj => obj.reference === `${code}`);
+            expect(validValues.length).to.equal(r.body.length);
+          }).should.return200OnGet();
+        return cloud.withOptions({ qs: { void_reason: `Temporary Reason` } }).delete(`${test.api}/${id}`)
+      });
   });
-  test
-    .withName(`should support searching ${test.api} by reference`)
-    .withOptions({ qs: { where: `search ='${code}'` } })
-    .withValidation((r) => {
-      expect(r).to.have.statusCode(200);
-      const validValues = r.body.filter(obj => obj.reference === `${code}`);
-      expect(validValues.length).to.equal(r.body.length);
-    }).should.return200OnGet();
+
 });
