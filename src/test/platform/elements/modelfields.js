879214d9c76cda36fe0a1bf48ec1886688ef11f3
Martin Brandt <martin@cloud-elements.com>
2018-03-20 11:20:14 -0600

    adding test for field method PUT

--- src/test/platform/elements/modelfields.js
+++ src/test/platform/elements/modelfields.js
@@ -2,16 +2,20 @@
 
 const suite = require('core/suite');
 const cloud = require('core/cloud');
+const expect = require('chakram').expect;
 const fieldSchema = require('./assets/element.elementmodelfield.schema.json');
 const modelpayload = require('core/tools').requirePayload(`${__dirname}/assets/element.elementmodel.payload.json`);
 const fieldPayload = require('core/tools').requirePayload(`${__dirname}/assets/element.elementmodelfield.payload.json`);
+const R = require('ramda');
 
-const putReadObject = (url, payload) => {
+
+const putReadObject = (url, payload, additionalProcessing) => {
   let object;
   return cloud
     .put(url, payload, fieldSchema)
     .then(r => (object = r.body[0]))
     .then(r => cloud.get(url + '/' + object.id))
+    .then(r => (additionalProcessing ? additionalProcessing(url, r.body) : r.body))
     .then(r =>
       cloud.put(
         url,
@@ -24,6 +28,17 @@ const putReadObject = (url, payload) => {
     );
 };
 
+const updateAndValidateFieldMethod = (url, payload) => {
+  const ogFieldMethodCount = payload.fieldMethods.length;
+  const newPayload = {data: [patchModelField(payload)], commitMessage: "patch it"};
+  const newFieldMethodCount = newPayload.data[0].fieldMethods.length;
+  expect(ogFieldMethodCount).to.not.equal(newFieldMethodCount)
+  return cloud.put(url, newPayload).then(r => {  
+    expect(R.head(r.body).fieldMethods.length).to.equal(newFieldMethodCount); 
+    return r;
+  });
+}
+
 const genObject = opts => {
   let newPayload = fieldPayload;
   if (opts.relatedModelId) {
@@ -31,6 +46,12 @@ const genObject = opts => {
   }
   return newPayload;
 };
+
+const patchModelField = modelField => {
+  const mf = R.assoc('fieldMethods', R.dropLast(1, R.path(['fieldMethods'], modelField)), modelField);
+  return mf;
+}
+
 
 suite.forPlatform('elements/modelfields', {payload: fieldPayload, schema: fieldSchema}, function(test) {
   let element, keyUrl, idUrl, modelId, idUrlWithModel, newModelId, keyUrlWithModel;
@@ -61,6 +82,12 @@ suite.forPlatform('elements/modelfields', {payload: fieldPayload, schema: fieldS
   it('should support RU for fields', function() {
     return putReadObject(idUrlWithModel + '/fields', genObject({relatedModelId: newModelId})).then(function() {
       return putReadObject(keyUrlWithModel + '/fields', genObject({relatedModelId: modelId}));
+    });
+  });
+
+   it('should support RU for field methods', function() {
+    return putReadObject(idUrlWithModel + '/fields', genObject({})).then(function() {
+      return putReadObject(keyUrlWithModel + '/fields', genObject({}), updateAndValidateFieldMethod);
     });
   });
 });
