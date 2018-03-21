5c803a472418aa3a17e01d3824353bdd0ec20c8a
Mrinal Mittal <GS-1445@GSLAB.COM>
2018-03-21 13:02:37 +0530

    Autotask Helpdesk: Added test case and payloads for Accounts resource

---
+++ src/test/elements/autotaskhelpdesk/accounts.js
@@ -0,0 +1,11 @@
+'use strict';
+
+const suite = require('core/suite');
+const payload = require('./assets/accounts');
+
+suite.forElement('helpdesk', 'accounts', { payload: payload }, (test) => {
+  test.should.supportCrus();
+  test.withOptions({ qs: { where: 'accountName=\'churrosTestAccount\'' } }).should.return200OnGet();
+  test.should.supportPagination();
+  test.should.supportNextPagePagination(1);
+ });
