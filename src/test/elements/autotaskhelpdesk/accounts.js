64592d99ea41d4d7a41bb9c790d321b517c86fec
Mrinal Mittal <GS-1445@GSLAB.COM>
2018-03-22 14:49:09 +0530

    Added validation for where clause

--- src/test/elements/autotaskhelpdesk/accounts.js
+++ src/test/elements/autotaskhelpdesk/accounts.js
@@ -1,11 +1,18 @@
 'use strict';
 
 const suite = require('core/suite');
+const cloud = require('core/cloud');
 const payload = require('./assets/accounts');
+const expect = require('chakram').expect;
 
 suite.forElement('helpdesk', 'accounts', { payload: payload }, (test) => {
   test.should.supportCrus();
-  test.withOptions({ qs: { where: 'accountName=\'churrosTestAccount\'' } }).should.return200OnGet();
+  test.withOptions({ qs: { where: 'accountName=\'churrosTestAccount\'' } })
+      .withValidation((r) => {
+        expect(r).to.have.statusCode(200);
+        const validValues = r.body.filter(obj => obj.accountName = 'churrosTestAccount');
+        expect(validValues.length).to.equal(r.body.length);
+      }).should.return200OnGet();
   test.should.supportPagination();
   test.should.supportNextPagePagination(1);
  });
