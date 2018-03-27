b0a17fdf4e68ae5fa27d8a2ca40b476d055d494f
wyselm <laura@cloud-elements.com>
2018-03-27 11:54:05 -0600

    add config

--- src/test/platform/vdrs/vdrs.js
+++ src/test/platform/vdrs/vdrs.js
@@ -6,12 +6,12 @@ const R = require('ramda');
 
 const payload = require('core/tools').requirePayload(`${__dirname}/assets/vdr.json`);
 const schema = require('core/tools').requirePayload(`${__dirname}/assets/vdr.schema.json`);
-const listSchema = require('core/tools').requirePayload(`${__dirname}/assets/vdrs.schema.json`);
-listSchema.definitions.vdr = schema;
+const pluralSchema = require('core/tools').requirePayload(`${__dirname}/assets/vdrs.schema.json`);
+pluralSchema.definitions.vdr = schema;
 
 suite.forPlatform('vdrs', {payload, schema}, test => {
 
   // NOTE - you need the 'vdrAdmin' role to run these tests
   test.withOptions({churros: {updatePayload: R.assoc('objectName', 'updatedObjectName2', payload)}}).should.supportCrud(chakram.put);
-  test.withValidation(listSchema).should.return200OnGet();
+  test.withValidation(pluralSchema).should.return200OnGet();
 });
