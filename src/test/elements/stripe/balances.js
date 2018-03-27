7416e4cdedd3657b0ef9394b7a570c3d72d751c2
madhuri.kadam <madhuri.kadam@gslab.com>
2018-03-27 13:27:11 +0530

    Added withvalidation

--- src/test/elements/stripe/balances.js
+++ src/test/elements/stripe/balances.js
@@ -1,7 +1,9 @@
 'use strict';
 
+const expect = require('chakram').expect;
 const suite = require('core/suite');
 
 suite.forElement('payment', 'balances', (test) => {
-  test.should.return200OnGet();
+  test.withValidation((r) => expect(r).to.have.statusCode(200))
+  .should.return200OnGet();
 });
