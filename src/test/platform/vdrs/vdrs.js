a2bf53967661f752fc0ce74c1d279356168465df
wyselm <laura@cloud-elements.com>
2018-03-28 14:18:31 -0600

    update vdr update test

--- src/test/platform/vdrs/vdrs.js
+++ src/test/platform/vdrs/vdrs.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const suite = require('core/suite');
+const cloud = require('core/cloud');
 const chakram = require('chakram');
 const R = require('ramda');
 
@@ -11,7 +12,33 @@ pluralSchema.definitions.vdr = schema;
 
 suite.forPlatform('vdrs', {payload, schema}, test => {
 
+  // Adds the correct id to the vdr field by matching on the path
+  const addMatchingVdrFieldId = (createdFields, field) => {
+    const matchingField = R.find(R.propEq('path', field.path))(createdFields);
+    return R.assoc('id', matchingField.id)(field);
+  };
+
+  const genUpdatePayload = (payload, newFields) => {
+    let up = R.assoc('objectName', 'updatedObjectName2', payload);
+    up.fields = newFields;
+    up.fields = R.dropLast(1, up.fields);
+    up.fields[0].path = 'anUpdateField';
+    up.fields.push({type: 'string', path: 'aNewField', level: 'system'});
+    return up;
+  };
+
   // NOTE - you need the 'vdrAdmin' role to run these tests
-  test.withOptions({churros: {updatePayload: R.assoc('objectName', 'updatedObjectName2', payload)}}).should.supportCrud(chakram.put);
-  test.withValidation(pluralSchema).should.return200OnGet();
+  it('should support CRUDS for VDRs', () => {
+    let vdrId, updatePayload;
+    return cloud.post('/vdrs', payload, schema)
+        .then(r => {
+            vdrId = r.body.id;
+            updatePayload = genUpdatePayload(payload, r.body.fields);
+            console.log(updatePayload);
+        })
+        .then(() => cloud.get(`/vdrs/${vdrId}`, schema))
+        .then(() => cloud.get(`/vdrs`, pluralSchema))
+        .then(() => cloud.put(`/vdrs/${vdrId}`, updatePayload, schema))
+        .then(() => cloud.delete(`/vdrs/${vdrId}`));
+  })
 });
