8f29c0526f180b8af6a257864427767535504dfd
Mayur Dhande <mayur.dhande@gslab.com>
2018-03-30 18:28:35 +0530

    test for metadata API

---
+++ src/test/elements/sugarcrmv2/metadata.js
@@ -0,0 +1,36 @@
+'use strict';
+const suite = require('core/suite');
+const expect = require('chakram').expect;
+var objects = [
+  "accountsNotes",
+  "leadsNotes",
+  "campaigns",
+  "incidents",
+  "campaignsNotes",
+  "incidentsNotesAttachments",
+  "tasks",
+  "incidentsHistory",
+  "incidentsNotes",
+  "accountsActivities",
+  "contactsActivities",
+  "opportunities",
+  "users",
+  "activities",
+  "leads",
+  "activitiesRaw",
+  "accounts",
+  "opportunitiesNotes",
+  "contacts",
+  "contactsNotes"
+];
+
+objects.forEach(obj => {
+  suite.forElement('finance', `objects/${obj}/metadata`, (test) => {
+    test.should.supportS();
+    test.withApi(test.api)
+      .withOptions({ qs: { customFieldsOnly: true } })
+      .withValidation(r => expect(r.body.fields.filter(field => (field.vendorPath.endsWith("_c") && field.custom === true))))
+      .withName(`should support return only custom fields for ${obj}`)
+      .should.return200OnGet();
+  });
+});
