8eb902c3175678a706b9caa2ff1a101314adc6da
Garima Agarwal <GS-1343@GSLAB.COM>
2018-03-30 12:47:07 +0530

    Modified affiliates

--- src/test/elements/infusionsoftrest/affiliates.js
+++ src/test/elements/infusionsoftrest/affiliates.js
@@ -5,13 +5,4 @@ const suite = require('core/suite');
 suite.forElement('crm', 'affiliates/commissions', (test) => {
   test.should.supportS();
   test.should.supportPagination();
-   test
-    .withOptions({ qs: { where: 'since=\'2018-03-22T07:09:53.000Z\'' } })
-    .withName('should support search by created_date')
-    .withValidation(r => {
-      expect(r).to.statusCode(200);
-      const validValues = r.body.filter(obj => obj.creation_date = '2018-03-22T07:09:53.000Z');
-      expect(validValues.length).to.equal(r.body.length);
-    })
-    .should.return200OnGet();
 });
