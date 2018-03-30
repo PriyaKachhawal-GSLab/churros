16014b9dbc3d31978ce881270a61ada0f9e55cbc
Taylor Clark <taylor@cloud-elements.com>
2018-03-30 13:46:27 -0500

    added tests for objects apis

---
+++ src/test/elements/quickbooks/objects.js
@@ -0,0 +1,18 @@
+'use strict';
+
+const expect = require('chakram').expect;
+const suite = require('core/suite');
+const cloud = require('core/cloud');
+
+suite.forElement('finance', 'objects', null, (test) => {
+  it('should support GET /objects', () => {
+    return cloud.get(test.api);
+  })
+
+  it('should support GET /objects/:objectName/metadata for all objects', () => {
+    let objects;
+    return cloud.get(test.api)
+    .then(r => objects = r.body)
+    .then(r => Promise.all(objects.map(obj => cloud.get(`${test.api}/${obj}/metadata`))))
+  });
+})
