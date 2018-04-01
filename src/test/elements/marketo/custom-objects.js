cd424916a6535db7a80e1f32a5da5d9f44ccedec
AfrinChakure-GSLab <afrin.chakure@gslab.com>
2018-04-01 20:46:15 +0530

    formatted code

--- src/test/elements/marketo/custom-objects.js
+++ src/test/elements/marketo/custom-objects.js
@@ -10,27 +10,27 @@ suite.forElement('marketing', 'custom-objects', { payload: payload }, (test) => 
  
   let customObjectName = 'afrinCustom_c', id;
   it('should allow R for /custom-objects', () => {
-	  const options = { qs: {   where: `names='leadActivities_c'`} };
-	  return cloud.withOptions(options).get(test.api);
+	 const options = { qs: {   where: `names='leadActivities_c'`} };
+	 return cloud.withOptions(options).get(test.api);
   });
   it('should allow S for /custom-objects{customObjectName}/custom-fields', () =>{
-	  const options = { qs: { where: `marketoGUID='c244523c-0a21-4c56-aa71-a845af615d4c,dab80af7-849f-47da-97af-cb3160867892, a3305990-a7e6-46f4-9a5b-9f81ae5db16c'`} };
-	  return cloud.withOptions(options).get(`${test.api}/${customObjectName}/custom-fields`);
+	 const options = { qs: { where: `marketoGUID='c244523c-0a21-4c56-aa71-a845af615d4c,dab80af7-849f-47da-97af-cb3160867892, a3305990-a7e6-46f4-9a5b-9f81ae5db16c'`} };
+	 return cloud.withOptions(options).get(`${test.api}/${customObjectName}/custom-fields`);
   });
   it('should allow paginating with page and nextPage /custom-objects{customObjectName}/custom-fields', () => {
      const options = { qs: { pageSize: 1 ,  where: `marketoGUID='c244523c-0a21-4c56-aa71-a845af615d4c,dab80af7-849f-47da-97af-cb3160867892'`} };
      return cloud.withOptions(options).get(`${test.api}/${customObjectName}/custom-fields`)
       .then(r => {
-        expect(r.body).to.not.be.null;
-        options.qs.nextPage = r.response.headers['elements-next-page-token'];
-        return cloud.withOptions(options).get(`${test.api}/${customObjectName}/custom-fields`);
-      });
+       expect(r.body).to.not.be.null;
+       options.qs.nextPage = r.response.headers['elements-next-page-token'];
+       return cloud.withOptions(options).get(`${test.api}/${customObjectName}/custom-fields`);
+     });
   });
   it('should allow CRUD for /custom-objects/{customObjectName}/custom-fields', () =>{
      const updatePayload = () => ({
         "input" : [{
 	    "afrinchaks" : tools.random()
-       }]
+      }]
      });
 	 return cloud.post(`${test.api}/${customObjectName}/custom-fields`, payload)
 	  .then(r => id = r.body[0].marketoGUID)
@@ -39,7 +39,7 @@ suite.forElement('marketing', 'custom-objects', { payload: payload }, (test) => 
 	  .then(r => cloud.delete(`${test.api}/${customObjectName}/custom-fields/${id}`));
   });
   it('should allow R for /custom-fields-templates', () =>{
-	  return cloud.get(`${test.api}/${customObjectName}/custom-fields-templates`);
+	 return cloud.get(`${test.api}/${customObjectName}/custom-fields-templates`);
   });
 });
 
