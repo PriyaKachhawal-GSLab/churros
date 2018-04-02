adec5e899339d52aa342e70001a129fa1f4c4f46
AbhayPore-GSLab <34128818+AbhayPore-GSLab@users.noreply.github.com>
2018-04-02 19:51:04 +0530

    Fixed CCI issue

--- src/test/elements/expensify/reports.js
+++ src/test/elements/expensify/reports.js
@@ -25,6 +25,6 @@ suite.forElement('payment', 'reports', { payload: payload }, (test) => {
   });
   it(`should support S for /{test.api}/reports/contents`, () => {
     return cloud.withOptions({ qs: { fileName: 'exportcfaa17d1-b738-4f69-b121-6f54eab40adb.txt' }}).get('/hubs/payment/reports/contents')
-	then(r => expect(r.body).to.not.be.empty);
+	.then(r => expect(r.body).to.not.be.empty);
   });
 });
