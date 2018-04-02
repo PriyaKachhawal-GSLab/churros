289edbccc07a0dd89cd228c81cc140fd2886851c
AfrinChakure-GSLab <afrin.chakure@gslab.com>
2018-04-02 10:39:03 +0530

    Formatted code 2

--- src/test/elements/marketo/custom-objects.js
+++ src/test/elements/marketo/custom-objects.js
@@ -7,40 +7,38 @@ const expect = require('chakram').expect;
 const payload = tools.requirePayload(`${__dirname}/assets/custom-objects.json`);
 
 suite.forElement('marketing', 'custom-objects', { payload: payload }, (test) => {
- 
-  let customObjectName = 'afrinCustom_c', id;
-  it('should allow R for /custom-objects', () => {
-	 const options = { qs: {   where: `names='leadActivities_c'`} };
-	 return cloud.withOptions(options).get(test.api);
-  });
-  it('should allow S for /custom-objects{customObjectName}/custom-fields', () =>{
-	 const options = { qs: { where: `marketoGUID='c244523c-0a21-4c56-aa71-a845af615d4c,dab80af7-849f-47da-97af-cb3160867892, a3305990-a7e6-46f4-9a5b-9f81ae5db16c'`} };
-	 return cloud.withOptions(options).get(`${test.api}/${customObjectName}/custom-fields`);
-  });
-  it('should allow paginating with page and nextPage /custom-objects{customObjectName}/custom-fields', () => {
-     const options = { qs: { pageSize: 1 ,  where: `marketoGUID='c244523c-0a21-4c56-aa71-a845af615d4c,dab80af7-849f-47da-97af-cb3160867892'`} };
-     return cloud.withOptions(options).get(`${test.api}/${customObjectName}/custom-fields`)
-      .then(r => {
-       expect(r.body).to.not.be.null;
-       options.qs.nextPage = r.response.headers['elements-next-page-token'];
-       return cloud.withOptions(options).get(`${test.api}/${customObjectName}/custom-fields`);
-     });
-  });
-  it('should allow CRUD for /custom-objects/{customObjectName}/custom-fields', () =>{
-     const updatePayload = () => ({
-        "input" : [{
-	    "afrinchaks" : tools.random()
-      }]
-     });
-	 return cloud.post(`${test.api}/${customObjectName}/custom-fields`, payload)
-	  .then(r => id = r.body[0].marketoGUID)
-	  .then(r => cloud.get(`${test.api}/${customObjectName}/custom-fields/${id}`))
-	  .then(r => cloud.patch(`${test.api}/${customObjectName}/custom-fields/${id}`, updatePayload()))
-	  .then(r => cloud.delete(`${test.api}/${customObjectName}/custom-fields/${id}`));
-  });
-  it('should allow R for /custom-fields-templates', () =>{
-	 return cloud.get(`${test.api}/${customObjectName}/custom-fields-templates`);
-  });
+	let customObjectName = 'afrinCustom_c', id;
+	it('should allow R for /custom-objects', () => {
+		const options = { qs: {   where: `names='leadActivities_c'`} };
+		return cloud.withOptions(options).get(test.api);
+	});
+	it('should allow S for /custom-objects{customObjectName}/custom-fields', () =>{
+		const options = { qs: { where: `marketoGUID='c244523c-0a21-4c56-aa71-a845af615d4c,dab80af7-849f-47da-97af-cb3160867892, a3305990-a7e6-46f4-9a5b-9f81ae5db16c'`} };
+		return cloud.withOptions(options).get(`${test.api}/${customObjectName}/custom-fields`);	
+	});
+	it('should allow paginating with page and nextPage /custom-objects{customObjectName}/custom-fields', () => {
+		const options = { qs: { pageSize: 1 ,  where: `marketoGUID='c244523c-0a21-4c56-aa71-a845af615d4c,dab80af7-849f-47da-97af-cb3160867892'`} };
+		return cloud.withOptions(options).get(`${test.api}/${customObjectName}/custom-fields`)
+		.then(r => {
+			expect(r.body).to.not.be.null;
+			options.qs.nextPage = r.response.headers['elements-next-page-token'];
+			return cloud.withOptions(options).get(`${test.api}/${customObjectName}/custom-fields`);
+		});
+	});
+	it('should allow CRUD for /custom-objects/{customObjectName}/custom-fields', () =>{
+		const updatePayload = () => ({
+			"input" : [{
+			"afrinchaks" : tools.random()
+			}]
+		});
+
+		return cloud.post(`${test.api}/${customObjectName}/custom-fields`, payload)
+		.then(r => id = r.body[0].marketoGUID)
+		.then(r => cloud.get(`${test.api}/${customObjectName}/custom-fields/${id}`))
+		.then(r => cloud.patch(`${test.api}/${customObjectName}/custom-fields/${id}`, updatePayload()))
+		.then(r => cloud.delete(`${test.api}/${customObjectName}/custom-fields/${id}`));
+	});
+	it('should allow R for /custom-fields-templates', () =>{
+		return cloud.get(`${test.api}/${customObjectName}/custom-fields-templates`);
+	});
 });
-
-
