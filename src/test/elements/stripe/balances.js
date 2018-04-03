135c51c56f33e110a740f610fba1ca623498ad64
madhuri.kadam <madhuri.kadam@gslab.com>
2018-04-03 18:55:33 +0530

    Updated the changes

--- src/test/elements/stripe/balances.js
+++ src/test/elements/stripe/balances.js
@@ -4,6 +4,9 @@ const expect = require('chakram').expect;
 const suite = require('core/suite');
 
 suite.forElement('payment', 'balances', (test) => {
-  test.withValidation((r) => expect(r).to.have.statusCode(200))
-  .should.return200OnGet();
+  test.withValidation((r) => {
+      expect(r).to.have.statusCode(200);
+      expect(r.body).to.be.an('Array')
+    })
+    .should.return200OnGet();
 });
