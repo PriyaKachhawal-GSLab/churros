e72daf37cb1e214c8d5308cd3e8e5fa3a298028d
madhuri.kadam <madhuri.kadam@gslab.com>
2018-04-03 19:01:43 +0530

    Updated the changes

--- src/test/elements/stripe/balances.js
+++ src/test/elements/stripe/balances.js
@@ -6,7 +6,7 @@ const suite = require('core/suite');
 suite.forElement('payment', 'balances', (test) => {
   test.withValidation((r) => {
       expect(r).to.have.statusCode(200);
-      expect(r.body).to.be.an('Array')
+      expect(r.body).to.be.an('Array');
     })
     .should.return200OnGet();
 });
