7108e44fb6d9ef4ef560b8dd4ad2d27b839b26db
seshuede <seshuede@gmail.com>
2018-04-06 00:53:40 -0500

    Enhanced Marketo with GET /objects/{objectName}/metadata API Testcases

---
+++ src/test/elements/marketo/metadata.js
@@ -0,0 +1,43 @@
+'use strict';
+const suite = require('core/suite');
+const cloud = require('core/cloud');
+const expect = require('chakram').expect;
+var objects = [
+  "afrinCustom_c",
+  "folders",
+  "externalActivityTypeAttributes",
+  "activityTypes",
+  "externalActivityType",
+  "externalActivityTypeApprove",
+  "campaigns",
+  "leadActivities_c",
+  "contactsMerge",
+  "company",
+  "externalActivityTypes",
+  "sampleBulkCustomObject_c",
+  "tagTypes",
+  "partitions",
+  "listsLeadsIsMember",
+  "filesContent",
+  "attendance_c",
+  "opportunity",
+  "linktest_c",
+  "channels",
+  "leads",
+  "lists",
+  "activities",
+  "aw_c",
+  "files",
+  "programsLeads",
+  "programs",
+  "readyTalkMeetingInfo_c"
+];
+
+suite.forElement('marketing', 'objects', (test) => {
+    return Promise.all(objects.map(obj => {
+        it(`should support GET /objects/${obj}/metadata`, () => {
+             return cloud.get(`${test.api}/${obj}/metadata`)
+            .then(r => expect(r.body.fields.filter(field => field.vendorPath)).to.not.be.empty);
+        });
+    }));
+});
