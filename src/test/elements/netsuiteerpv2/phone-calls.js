e20d178f57562469d58a79f4d663411bfc4e9fe5
Akash R Singh <GS-1670@GSLAB.COM>
2018-04-09 15:29:54 +0530

    Churros tests added for phone-calls resource

---
+++ src/test/elements/netsuiteerpv2/phone-calls.js
@@ -0,0 +1,11 @@
+'use strict';
+
+const suite = require('core/suite');
+const cloud = require('core/cloud');
+const phoneCallsPayload = require('./assets/phone-calls');
+
+suite.forElement('erp', 'phone-calls', { payload: phoneCallsPayload }, (test) => {
+  test.should.supportCruds();
+  test.withOptions({ qs: { page: 1, pageSize: 5 } }).should.supportPagination();
+  test.should.supportCeqlSearch('title');
+});
