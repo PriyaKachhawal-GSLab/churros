568b71007aa35326009c00d0ade6bbc0810a17de
Akash R Singh <GS-1670@GSLAB.COM>
2018-04-09 16:50:02 +0530

    Enhanced churros test for search in phone-calls resource

--- src/test/elements/netsuiteerpv2/phone-calls.js
+++ src/test/elements/netsuiteerpv2/phone-calls.js
@@ -1,10 +1,19 @@
 'use strict';
 
 const suite = require('core/suite');
+const expect = require('chakram').expect;
 const phoneCallsPayload = require('./assets/phone-calls');
 
 suite.forElement('erp', 'phone-calls', { payload: phoneCallsPayload }, (test) => {
   test.should.supportCruds();
   test.withOptions({ qs: { page: 1, pageSize: 5 } }).should.supportPagination();
-  test.should.supportCeqlSearch('title');
+  
+  test
+    .withName(`should support searching ${test.api} by title`)
+    .withOptions({ qs: { where: "title = 'PC10'" } })
+    .withValidation((r) => {
+      expect(r).to.have.statusCode(200);
+      const validValues = r.body.filter(obj => obj.title === 'PC10');
+      expect(validValues.length).to.equal(r.body.length);
+    }).should.return200OnGet();
 });
