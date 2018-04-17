111dc383798c413f1e5e059f445dd53db96eb9b9
Mayur Dhande <mayur.dhande@gslab.com>
2018-04-17 13:57:10 +0530

    separated tests

--- src/test/elements/chargify/product-families.js
+++ src/test/elements/chargify/product-families.js
@@ -13,14 +13,17 @@ suite.forElement('payment', 'product-families', (test) => {
     "description": "churros description",
     "handle": tools.random()
   };
-  test.withOptions({ qs: { where: 'direction=\'desc\''}}).should.return200OnGet();
-  it(`should allow GET for ${test.api}/{productFamilyId}`, () => {
+  test.withOptions({ qs: { where: 'direction=\'desc\'' } }).should.return200OnGet();
+  it(`should allow SR for ${test.api}/{productFamilyId}`, () => {
     let productFamilyId;
     return cloud.post(test.api, payload)
       .then(r => cloud.get(test.api))
       .then(r => productFamilyId = r.body[0].id)
-      .then(r => cloud.get(`${test.api}/${productFamilyId}`))
-      .then(r => cloud.post(`${test.api}/${productFamilyId}/products`, updatePayload));
+      .then(r => cloud.get(`${test.api}/${productFamilyId}`));
+  });
+  // skipping because no delete API is present for prdocuts resource
+  it.skip(`should allow POST for ${test.api}/{productFamilyId}/products`, () => {
+    return cloud.post(`${test.api}/${productFamilyId}/products`, updatePayload);
   });
   test.should.supportPagination();
 });
