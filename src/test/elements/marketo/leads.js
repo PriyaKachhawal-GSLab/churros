1ad711d1780bcd6aa4daa2907fc6e85e6480158e
AbhayPore-GSLab <34128818+AbhayPore-GSLab@users.noreply.github.com>
2018-04-25 19:24:58 +0530

    Added leads test suite

--- src/test/elements/marketo/leads.js
+++ src/test/elements/marketo/leads.js
@@ -1,15 +1,35 @@
 'use strict';
 
 const suite = require('core/suite');
+const tools = require('core/tools');
 const cloud = require('core/cloud');
+const expect = require('chakram').expect;
+const payload = tools.requirePayload(`${__dirname}/assets/leads.json`);
+const updatedPayload = tools.requirePayload(`${__dirname}/assets/leads.json`);
+const interactionPayload = {
+  "id": tools.randomInt(),
+  "token": tools.randomInt()
+};
 
-suite.forElement('marketing', 'programs', (test) => {
-  it('should allow R for programs/{id}/leads', () => {
-    let program;
-    return cloud.get(test.api)
-      .then(r => program = r.body[0])
-      .then(r => cloud.get(`${test.api}/${program.id}`));
+suite.forElement('marketing', 'leads', { payload: payload }, (test) => {
+  it('should allow CRUDS for /leads', () => {
+    let id;
+    return cloud.post(test.api, payload)
+      .then(r => id = r.body.person.id)
+      .then(r => cloud.get(`${test.api}/${id}`))
+      .then(r => cloud.patch(`${test.api}/${id}`, updatedPayload))
+      .then(r => cloud.withOptions({ qs: { where: `id in ( ${id} )` } }).get(test.api))
+      .then(r => cloud.delete(`${test.api}/${id}`));
+  });
+
+  it('should allow POST /leads/:id/merge and POST /leads/:id/interactions', () => {
+    let id, id2;
+    return cloud.post(test.api, payload)
+      .then(r => id = r.body.person.id)
+      .then(r => cloud.post(`${test.api}/${id}/interactions`, interactionPayload))
+      .then(r => cloud.post(test.api, updatedPayload))
+      .then(r => id2 = r.body.person.id)
+      .then(r => cloud.post(`${test.api}/${id}/merge`, { leadIds: [id2] }))
+      .then(r => cloud.delete(`${test.api}/${id}`));
   });
 });
-
-
