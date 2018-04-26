3adae04109c0af6b153b9d48ceb911e55702dff3
AfrinChakure-GSLab <afrin.chakure@gslab.com>
2018-04-26 22:57:14 +0530

    Corrected failing testcase for contacts

--- src/test/elements/connectwisecrmrest/contacts.js
+++ src/test/elements/connectwisecrmrest/contacts.js
@@ -7,14 +7,15 @@ const chakram = require('chakram');
 const options = {
   churros: {
     updatePayload: {
-      "firstName": "test_1"
+      "firstName": "test_1",
+      "portalPassword": "Churros@123"
     }
   }
 };
 
 suite.forElement('crm', 'contacts', { payload: payload }, (test) => {
-  test.should.supportCruds(chakram.put);            //test put
-  test.withOptions(options).should.supportCruds();  //test patch
+  test.withOptions(options).should.supportCruds(chakram.put); //test put
+  test.withOptions(options).should.supportCruds(); //test patch
   test.should.supportPagination('id');
   test.should.supportCeqlSearch('id');
   test.withName(`should support searching ${test.api} by email`).withOptions({ qs: { where: `email='Test'` } }).should.return200OnGet();
