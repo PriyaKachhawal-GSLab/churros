6539af0e2b34c4247d8ce0ba75b21ea3b62b94d6
Priya P Kachhawal <priya.kachhawal@gslab.com>
2018-05-07 19:00:25 +0530

    Test for new query param : attributes

--- src/test/elements/sageone/tax-rates.js
+++ src/test/elements/sageone/tax-rates.js
@@ -5,25 +5,30 @@ const tools = require('core/tools');
 const cloud = require('core/cloud');
 const payload = require('./assets/tax-rates');
 const chakram = require('chakram');
+const expect = chakram.expect;
 const build = (overrides) => Object.assign({}, payload, overrides);
-const taxRatesPayload = build({ reference: "re" + tools.randomInt() });
+const taxRatesPayload = build({
+  reference: "re" + tools.randomInt()
+});
 
-suite.forElement('finance', 'tax-rates', { payload: taxRatesPayload }, (test) => {
+suite.forElement('finance', 'tax-rates', {
+  payload: taxRatesPayload
+}, (test) => {
   test.should.supportCrus(chakram.put);
   test.should.supportPagination(1);
   it(`should support GET ${test.api}`, () => {
     return cloud.get(test.api);
   });
-    it.skip('should support CRUDS for tax-rates', () => {
-      let companyId;
-      return cloud.post(test.api, payload)
-        .then(r => id = r.body.id)
-        .then(r => cloud.withOptions({ qs: { fields: 'percentage,agency' } })
-          .get(`${test.api}/${id}`)
-          .then(r => {
-            expect(r.body).to.contain.key('percentage');
-            expect(r.body).to.contain.key('agency');
-          }));
-        });
+
+  test
+    .withOptions({
+      qs: {
+        attributes: 'percentage'
+      }
+    })
+    .withValidation((r) => {
+      expect(r).to.have.statusCode(200);
+      expect(r.body[0]).to.contain.key('percentage');
+    }).should.return200OnGet();
 });
 //where clause doesnot work
