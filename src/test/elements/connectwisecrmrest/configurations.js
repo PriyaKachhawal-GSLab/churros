5149fb7aa168f5dd0063be88243f1146eb277fa5
AfrinChakure-GSLab <afrin.chakure@gslab.com>
2018-05-09 19:35:59 +0530

    incorporate review comments

--- src/test/elements/connectwisecrmrest/configurations.js
+++ src/test/elements/connectwisecrmrest/configurations.js
@@ -5,17 +5,20 @@ const suite = require('core/suite');
 const tools = require('core/tools');
 const expect = require('chakram').expect;
 const oPayload = require('./assets/organizations');
-const build = (overrides) => Object.assign({}, oPayload, overrides);
-const orgPayload = build({ identifier: tools.randomStr('abcdefghijklmnopqrstuvwxyz0123456789', 10) });
 const incidentpayload = require('./assets/incidents');
 const configurationPayload = require('./assets/configurations');
 const incidentConfigurationPayload = require('./assets/incidentconfigurations');
 
+const createOrganization = (oPayload) => {
+  oPayload.identifier = tools.randomStr('abcdefghijklmnopqrstuvwxyz1234567890', 20);
+  return oPayload;
+};
+
 suite.forElement('crm', 'configurations', { payload: incidentpayload }, (test) => {
   test.should.supportPagination('id');
-  it(`should support CDS for /configurations and SU for /incidents/{id}/configurations`, () => {
-    let organizationId, incidentId, id;
-    return cloud.post(`/hubs/crm/organizations`, orgPayload)
+  let incidentId, organizationId;
+  before(() => {
+    return cloud.post(`/hubs/crm/organizations`, createOrganization(oPayload))
       .then(r => {
         organizationId = r.body.id;
         incidentpayload.company = { id: organizationId };
@@ -25,8 +28,12 @@ suite.forElement('crm', 'configurations', { payload: incidentpayload }, (test) =
         incidentId = r.body.id;
         configurationPayload.company = { id: organizationId };
         configurationPayload.name = tools.random();
-      })
-      .then(r => cloud.post(test.api, configurationPayload))
+      });
+  });
+
+  it(`should support CDS for /configurations and SU for /incidents/{id}/configurations`, () => {
+    let id;
+    return cloud.post(test.api, configurationPayload)
       .then(r => {
         id = r.body.id;
         incidentConfigurationPayload.id = id;
@@ -39,8 +46,10 @@ suite.forElement('crm', 'configurations', { payload: incidentpayload }, (test) =
         expect(r).to.have.statusCode(200);
         expect(r.body).to.have.lengthOf(1);
       })
-      .then(r => cloud.delete(`${test.api}/${id}`))
-      .then(r => cloud.delete(`/hubs/crm/incidents/${incidentId}`))
+      .then(r => cloud.delete(`${test.api}/${id}`));
+  });
+  after(() => {
+    return cloud.delete(`/hubs/crm/incidents/${incidentId}`)
       .then(r => cloud.delete(`/hubs/crm/organizations/${organizationId}`));
   });
 });
