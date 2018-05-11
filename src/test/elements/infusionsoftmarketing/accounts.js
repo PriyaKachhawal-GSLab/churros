acafe32b0868b30f1eb4ef436cec94aadedcb103
AfrinChakure-GSLab <afrin.chakure@gslab.com>
2018-05-11 17:04:14 +0530

    Adding GET leads and accounts

--- src/test/elements/infusionsoftmarketing/accounts.js
+++ src/test/elements/infusionsoftmarketing/accounts.js
@@ -5,4 +5,5 @@ const payload = require('./assets/accounts');
 
 suite.forElement('marketing', 'accounts', { payload: payload }, (test) => {
   test.should.supportCrud();
+  test.withOptions({qs: { where: 'FirstName=\'Churros\''}}).should.return200OnGet();
 });
--- src/test/elements/infusionsoftmarketing/leads.js
+++ src/test/elements/infusionsoftmarketing/leads.js
@@ -5,4 +5,5 @@ const payload = require('./assets/leads');
 
 suite.forElement('marketing', 'leads', { payload: payload }, (test) => {
   test.should.supportCrud();
+  test.withOptions({qs: { where: 'OpportunityTitle=\'Final\''}}).should.return200OnGet();
 });
