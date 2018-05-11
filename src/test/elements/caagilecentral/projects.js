c18a66183561c56a801806bccb576a2ca98f0fe7
GarimaAgarwal-GSLab <garima.agarwal@gslab.com>
2018-05-11 16:01:54 +0530

    Added test for new element

---
+++ src/test/elements/caagilecentral/projects.js
@@ -0,0 +1,20 @@
+'use strict';
+
+const suite = require('core/suite');
+const tools = require('core/tools');
+const expect = require('chakram').expect;
+
+
+suite.forElement('general', 'projects', (test) => {
+  test.should.supportSr();
+  test.should.supportPagination();
+  test
+    .withOptions({ qs: { where: 'State = \'Open\'' } })
+    .withName('should support search by filter')
+    .withValidation(r => {
+      expect(r).to.statusCode(200);
+      const validValues = r.body.filter(obj => obj.State = 'Open');
+      expect(validValues.length).to.equal(r.body.length);
+    })
+    .should.return200OnGet();
+});
