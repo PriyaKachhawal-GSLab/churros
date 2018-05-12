65d62e70bccdebfb57f8f6f30a345c9fbae8e97d
Atul Barve <atul@cloud-elements.com>
2018-05-12 08:32:58 -0500

    Fixed Churros Errors.

--- src/test/elements/dynamicscrmadfs/dynamic_list.js
+++ src/test/elements/dynamicscrmadfs/dynamic_list.js
@@ -1,9 +1,6 @@
 'use strict';
 
 const suite = require('core/suite');
-const build = (overrides) => Object.assign({}, payload, overrides);
-const chakram = require('chakram');
-const cloud = require('core/cloud');
 
 suite.forElement('crm', 'contacts', { }, (test) => {
   test.withOptions({ qs: { where : `listid = '8cc802ca-6154-e811-812e-e0071b715b91'` } }).should.supportPagination();
--- src/test/elements/dynamicscrmadfs/static_list.js
+++ src/test/elements/dynamicscrmadfs/static_list.js
@@ -1,9 +1,6 @@
 'use strict';
 
 const suite = require('core/suite');
-const build = (overrides) => Object.assign({}, payload, overrides);
-const chakram = require('chakram');
-const cloud = require('core/cloud');
 
 suite.forElement('crm', 'contacts', { }, (test) => {
   test.withOptions({ qs: { where : `listid = 'c6d384d2-5749-e811-8120-e0071b66cf61'` } }).should.supportPagination();
