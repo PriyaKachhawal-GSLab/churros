db40e6a81db82d35eb4249f2f93d4bd962e7b745
Priya P Kachhawal <priya.kachhawal@gslab.com>
2018-05-14 14:39:20 +0530

    Incorporated review comments

--- src/test/elements/infusionsoftcrm/contacts.js
+++ src/test/elements/infusionsoftcrm/contacts.js
@@ -1,9 +1,18 @@
 'use strict';
 
+const expect = require('chakram').expect;
 const suite = require('core/suite');
 const payload = require('./assets/contacts');
 
 suite.forElement('crm', 'contacts', { payload: payload }, (test) => {
   test.should.supportCrud();
-  test.withOptions({qs: { where: 'Email=\'senior.churros@cloud-elements.com\''}}).should.return200OnGet();
+  //test.withOptions({ qs: { where: 'Email=\'senior.churros@cloud-elements.com\'' } }).should.return200OnGet();
+  test.withOptions({ qs: { where: 'Email=\'senior.churros@cloud-elements.com\'' } })
+    .withName('should support search by filter')
+    .withValidation(r => {
+      expect(r).to.statusCode(200);
+      const validValues = r.body.filter(obj => obj.Email = 'senior.churros@cloud-elements.com');
+      expect(validValues.length).to.equal(r.body.length);
+    })
+    .should.return200OnGet();
 });
