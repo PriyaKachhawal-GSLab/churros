cfbc1e6b8d4cb5814ba614d458e01b57734d6c40
AfrinChakure-GSLab <afrin.chakure@gslab.com>
2018-05-14 14:37:08 +0530

    incorporated review comments

--- src/test/elements/infusionsoftmarketing/accounts.js
+++ src/test/elements/infusionsoftmarketing/accounts.js
@@ -2,8 +2,18 @@
 
 const suite = require('core/suite');
 const payload = require('./assets/accounts');
+const chakram = require('chakram');
+const expect = chakram.expect;
 
 suite.forElement('marketing', 'accounts', { payload: payload }, (test) => {
   test.should.supportCrud();
-  test.withOptions({qs: { where: 'FirstName=\'Churros\''}}).should.return200OnGet();
+  test
+    .withOptions({ qs: { where: 'FirstName=\'Churros\'' } })
+    .withName('should support search by filter')
+    .withValidation(r => {
+      expect(r).to.statusCode(200);
+      const validValues = r.body.filter(obj => obj.State = 'FirstName');
+      expect(validValues.length).to.equal(r.body.length);
+    })
+    .should.return200OnGet();
 });
--- src/test/elements/infusionsoftmarketing/leads.js
+++ src/test/elements/infusionsoftmarketing/leads.js
@@ -2,8 +2,18 @@
 
 const suite = require('core/suite');
 const payload = require('./assets/leads');
+const chakram = require('chakram');
+const expect = chakram.expect;
 
 suite.forElement('marketing', 'leads', { payload: payload }, (test) => {
   test.should.supportCrud();
-  test.withOptions({qs: { where: 'OpportunityTitle=\'Final\''}}).should.return200OnGet();
+  test
+    .withOptions({ qs: { where: 'OpportunityTitle=\'Final\'' } })
+    .withName('should support search by filter')
+    .withValidation(r => {
+      expect(r).to.statusCode(200);
+      const validValues = r.body.filter(obj => obj.State = 'OpportunityTitle');
+      expect(validValues.length).to.equal(r.body.length);
+    })
+    .should.return200OnGet();
 });
