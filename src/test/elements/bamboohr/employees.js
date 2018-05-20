9543dfb6e84cb6225e079eee0d6faa288386d4c8
chinnababu <chinnapave@gmail.com>
2018-05-20 16:37:58 -0500

    Incorporated review comments

--- src/test/elements/bamboohr/employees.js
+++ src/test/elements/bamboohr/employees.js
@@ -8,27 +8,28 @@ const attachmentUpdatePayload = tools.requirePayload(`${__dirname}/assets/update
 suite.forElement('humancapital', 'employees', { payload: payload }, (test) => {
     let empId;
     let categoryId;
-
     before(() => {
         return cloud.get('employees')
             .then(r => empId = r.body[0].id)
-            .then(r => cloud.get(`${test.api}/${empId}/categories/attachments`))
+            .then(r => cloud.get(`${test.api}/${empId}/categories`))
             .then(r => categoryId = r.body[0].id);
     });
+
     test.should.supportCrus();
+
     it(`should allow C for ${test.api}/categories`, () => {
         return cloud.post(`${test.api}/categories`, categoriesPayload);
     });
-    let txtFile = __dirname + '/assets/History.txt';
 
     it(`should allow Cruds for ${test.api}/:empId/categories/:categoryId/attachments`, () => {
         let attachmentId;
         let query = { fileName: tools.random(), share: 'yes' };
+        let txtFile = __dirname + '/assets/History.txt';
         return cloud.withOptions({ qs: query }).postFile(`${test.api}/${empId}/categories/${categoryId}/attachments`, txtFile)
             .then(r => attachmentId = r.body.id)
             .then(r => cloud.get(`${test.api}/${empId}/categories/${categoryId}/attachments/${attachmentId}`))
             .then(r => cloud.patch(`${test.api}/${empId}/categories/${categoryId}/attachments/${attachmentId}`, attachmentUpdatePayload))
             .then(r => cloud.delete(`${test.api}/${empId}/categories/${categoryId}/attachments/${attachmentId}`))
-            .then(r => cloud.get(`${test.api}/${empId}/categories/attachments`));
+            .then(r => cloud.get(`${test.api}/${empId}/categories`));
     });
 });
