13232d8a6dd97f52d302985fd65144ce78dc71ba
AbhayPore-GSLab <34128818+AbhayPore-GSLab@users.noreply.github.com>
2018-05-28 12:02:41 +0530

    incorporated review comments

--- src/test/elements/hootsuite/interactions.js
+++ src/test/elements/hootsuite/interactions.js
@@ -1,10 +1,12 @@
 'use strict';
-
+const expect = require('chakram').expect;
 const suite = require('core/suite');
 const cloud = require('core/cloud');
 
 suite.forElement('social', 'interactions', (test) => {
   
   it(`should allow GET for ${test.api}`, () => {
-    return cloud.withOptions({ qs: { where: "socialNetworkType='Twitter' and socialNetworkId=82295100 and targetSocialNetworkId=876713392311902208" } }).get(`${test.api}`);});
+    return cloud.withOptions({ qs: { where: "socialNetworkType='Twitter' and socialNetworkId=82295100 and targetSocialNetworkId=876713392311902208" } }).get(`${test.api}`)
+	.then(r => expect(r.body[0].socialNetworkType).to.contain('TWITTER'));
+	});
 });
