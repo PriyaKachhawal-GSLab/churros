10a4380df12d1aeddb906b773ac16bbefac8ab8c
Prajakta A Kalyankar <GS-1714@GSLAB.COM>
2018-06-01 13:54:40 +0530

    Incorporated review comments

--- src/test/elements/connectwisecrmrest/opportunities.js
+++ src/test/elements/connectwisecrmrest/opportunities.js
@@ -1,7 +1,6 @@
 'use strict';
 
 const suite = require('core/suite');
-const cloud = require('core/cloud');
 const payload = require('./assets/opportunities');
 
 suite.forElement('crm', 'opportunities', { payload: payload }, (test) => {
