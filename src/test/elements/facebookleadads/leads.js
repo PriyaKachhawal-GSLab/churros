b264aac0506ee0efd9c64880dc13a994ae2247ce
AfrinChakure-GSLab <afrin.chakure@gslab.com>
2018-06-04 19:38:52 +0530

    Added testcase for GET /forms/{formId}/leads in facebookleadads

--- src/test/elements/facebookleadads/leads.js
+++ src/test/elements/facebookleadads/leads.js
@@ -4,6 +4,14 @@ const suite = require('core/suite');
 const cloud = require('core/cloud');
 
 suite.forElement('marketing', 'leads', null, (test) => {
+  //Currently we have this formId available with Leads, hence hardcoding the same.
+  //Used https://developers.facebook.com/tools/lead-ads-testing to create leads
+  //Value of formLeadId will change if you delete the existing lead.
+  let formId = 1856949527939272;
+  it('should allow R for /hubs/marketing/forms/{formId}/leads', () => {
+    return cloud.get(`/hubs/marketing/forms/${formId}/leads`)
+      .then(r => cloud.withOptions({ qs: { fields: 'created_time,form_id' } }).get(`/hubs/marketing/forms/${formId}/leads`));
+  });
   // As we do not have a GET all API for leads, hardcoding the leadId
   let leadId = 239907496137483;
   it('should allow R for /hubs/marketing/leads', () => {
