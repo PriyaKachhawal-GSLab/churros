e5c17842ded9b5c9267489352256b023aa1855bc
Prajakta A Kalyankar <GS-1714@GSLAB.COM>
2018-06-14 13:40:30 +0530

    Incorporated review comments

--- src/test/elements/servicenowoauth/items.js
+++ src/test/elements/servicenowoauth/items.js
@@ -13,6 +13,6 @@ suite.forElement('helpdesk', 'items', (test) => {
   it('should allow C for /hubs/helpdesk/items/{sys_id}/submit_producer', () => {
     let sys_id = "01205b180a0a0b3000b6efd641d24b75";
     return cloud.post(`${test.api}/${sys_id}/submit_producer`, payload)
-	  .then(r => expect(r.body).to.not.be.empty);
+      .then(r =>  expect(r).to.have.statusCode(200));
   });
 });
