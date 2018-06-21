590a5fe5c060079a65f4c91fa1bfbf45ee03a811
kallurisandeep <sandeepcelements@gmail.com>
2018-06-21 17:02:22 -0500

    Fixed brighttalk churros issue

--- src/test/elements/brighttalk/channels.js
+++ src/test/elements/brighttalk/channels.js
@@ -13,33 +13,28 @@ suite.forElement('marketing', 'channels', null, (test) => {
   });
 
   it(`should support GET, pagination and Ceql search for ${test.api}/:channelId/activities`, () => {
-    let id;
     return cloud.get(`${test.api}/${channelId}/activities`)
-      .then(r => id = r.body[0].id)
       .then(r => cloud.withOptions({ qs: { page: 1, pageSize: 1 } }).get(`${test.api}/${channelId}/activities`))
-      .then(r => cloud.withOptions({ qs: { where: `id='${id}'` } }).get(`${test.api}/${channelId}/activities`));
   });
 
   it('should support GET, pagination and Ceql search for /hubs/marketing/channels/:channelId/contacts', () => {
-    let id;
-    return cloud.get(`${test.api}/${channelId}/contacts`)
-      .then(r => id = r.body[0].id)
+    
+    return cloud.get(`${test.api}/${channelId}/contacts`)     
       .then(r => cloud.withOptions({ qs: { page: 1, pageSize: 1 } }).get(`${test.api}/${channelId}/contacts`))
-      .then(r => cloud.withOptions({ qs: { where: `id='${id}'` } }).get(`${test.api}/${channelId}/contacts`));
+      
   });
 
   it(`should support GET, pagination and Ceql search for ${test.api}/:channelId/subscriberWebcastAttachmentAccessSummaries`, () => {
-    let id;
+    
     return cloud.get(`${test.api}/${channelId}/subscriberWebcastAttachmentAccessSummaries`)
-      .then(r => id = r.body[0].id)
       .then(r => cloud.withOptions({ qs: { page: 1, pageSize: 1 } }).get(`${test.api}/${channelId}/subscriberWebcastAttachmentAccessSummaries`))
-      .then(r => cloud.withOptions({ qs: { where: `id='${id}'` } }).get(`${test.api}/${channelId}/subscriberWebcastAttachmentAccessSummaries`));
+      
   });
 
   it(`should support GET, pagination and Ceql search for ${test.api}/:channelId/surveys  and /hubs/marketing/surveys/:surveyId/responses`, () => {
-    let surveyId, id;
+    
     return cloud.get(`${test.api}/${channelId}/surveys`)
-      .then(r => surveyId = r.body[0].id)
+      
       .then(r => cloud.withOptions({ qs: { page: 1, pageSize: 1 } }).get(`${test.api}/${channelId}/surveys`))
       .then(r => cloud.withOptions({ qs: { where: `id='${surveyId}'` } }).get(`${test.api}/${channelId}/surveys`))
       .then(r => cloud.get(`/hubs/marketing/surveys/${surveyId}/responses`))
