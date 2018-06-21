aa7abf9b3d80aaf07edff4e84becad2182307747
kallurisandeep <sandeepcelements@gmail.com>
2018-06-21 17:23:56 -0500

    made changes according to ci circle erros

--- src/test/elements/brighttalk/channels.js
+++ src/test/elements/brighttalk/channels.js
@@ -14,27 +14,26 @@ suite.forElement('marketing', 'channels', null, (test) => {
 
   it(`should support GET, pagination and Ceql search for ${test.api}/:channelId/activities`, () => {
     return cloud.get(`${test.api}/${channelId}/activities`)
-      .then(r => cloud.withOptions({ qs: { page: 1, pageSize: 1 } }).get(`${test.api}/${channelId}/activities`))
+      .then(r => cloud.withOptions({ qs: { page: 1, pageSize: 1 } }).get(`${test.api}/${channelId}/activities`));
   });
 
   it('should support GET, pagination and Ceql search for /hubs/marketing/channels/:channelId/contacts', () => {
     
     return cloud.get(`${test.api}/${channelId}/contacts`)     
-      .then(r => cloud.withOptions({ qs: { page: 1, pageSize: 1 } }).get(`${test.api}/${channelId}/contacts`))
+      .then(r => cloud.withOptions({ qs: { page: 1, pageSize: 1 } }).get(`${test.api}/${channelId}/contacts`));
       
   });
 
   it(`should support GET, pagination and Ceql search for ${test.api}/:channelId/subscriberWebcastAttachmentAccessSummaries`, () => {
     
     return cloud.get(`${test.api}/${channelId}/subscriberWebcastAttachmentAccessSummaries`)
-      .then(r => cloud.withOptions({ qs: { page: 1, pageSize: 1 } }).get(`${test.api}/${channelId}/subscriberWebcastAttachmentAccessSummaries`))
+      .then(r => cloud.withOptions({ qs: { page: 1, pageSize: 1 } }).get(`${test.api}/${channelId}/subscriberWebcastAttachmentAccessSummaries`));
       
   });
 
   it(`should support GET, pagination and Ceql search for ${test.api}/:channelId/surveys  and /hubs/marketing/surveys/:surveyId/responses`, () => {
-    
+    let surveyId,id;
     return cloud.get(`${test.api}/${channelId}/surveys`)
-      
       .then(r => cloud.withOptions({ qs: { page: 1, pageSize: 1 } }).get(`${test.api}/${channelId}/surveys`))
       .then(r => cloud.withOptions({ qs: { where: `id='${surveyId}'` } }).get(`${test.api}/${channelId}/surveys`))
       .then(r => cloud.get(`/hubs/marketing/surveys/${surveyId}/responses`))
