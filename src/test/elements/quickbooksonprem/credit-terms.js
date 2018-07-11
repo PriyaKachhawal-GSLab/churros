29b5d404696d5245794792155cec3f076c462f6e
kallurisandeep <sandeepcelements@gmail.com>
2018-07-11 17:49:13 -0500

    added new test case

--- src/test/elements/quickbooksonprem/credit-terms.js
+++ src/test/elements/quickbooksonprem/credit-terms.js
@@ -16,10 +16,10 @@ suite.forElement('finance', 'credit-terms', (test) => {
   
   it(`should return an error when 'TimeModified' filter is not a proper Date`, () => {
     return cloud.withOptions({qs: {where: `TimeModified='2018'`}})
-      .get(test.api, (r) => expect(r).to.have.statusCode(400))
+      .get(test.api, (r) => expect(r).to.have.statusCode(400));
   });
   it(`should return an error when 'active' filter is not true or false`, () => {
     return cloud.withOptions({qs: {where: `active='isNotTrueOrFalse'`}})
-      .get(test.api, (r) => expect(r).to.have.statusCode(400))
+      .get(test.api, (r) => expect(r).to.have.statusCode(400));
   });
 });
