4329053bbc0f91638b2386352d370efd5371eaf2
kallurisandeep <sandeepcelements@gmail.com>
2018-07-16 12:53:37 -0500

    added churros test cases for greatplains

---
+++ src/test/elements/greatplains/tax-rates.js
@@ -0,0 +1,13 @@
+'use strict';
+const suite = require('core/suite');
+const expect = require('chakram').expect;
+suite.forElement('finance', 'tax-rates', (test) => {
+  test.should.supportPagination();
+  test.should.supportSr();
+    test.withApi(test.api)
+    .withOptions({ qs: { where: `taxDetailKeyId='AUSSTE+PS0N0'` } })
+    .withValidation(r => expect(r.body.filter(obj => obj.taxDetailKey.id !== "")).to.not.be.empty)
+    .withName('should allow GET with option taxDetailKeyId')
+    .should.return200OnGet();
+    
+  });
