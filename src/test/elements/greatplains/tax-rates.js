7a79682aed4bec3406dd59f5f9d705c9ead79741
kallurisandeep <sandeepcelements@gmail.com>
2018-07-17 10:29:29 -0500

    made changes according to git comments

--- src/test/elements/greatplains/tax-rates.js
+++ src/test/elements/greatplains/tax-rates.js
@@ -4,7 +4,7 @@ const expect = require('chakram').expect;
 suite.forElement('finance', 'tax-rates', (test) => {
   test.should.supportPagination();
   test.should.supportSr();
-    test.withApi(test.api)
+  test.withApi(test.api)
     .withOptions({ qs: { where: `taxDetailKeyId='AUSSTE+PS0N0'` } })
     .withValidation(r => expect(r.body.filter(obj => obj.taxDetailKey.id !== "")).to.not.be.empty)
     .withName('should allow GET with option taxDetailKeyId')
