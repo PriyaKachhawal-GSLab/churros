266412814363a2a9c81f0ae1ab39c66ff50f85b0
wyselm <laura@cloud-elements.com>
2018-07-30 14:39:27 -0600

    add test for js transformation

--- src/test/platform/vdrs/upgrades.js
+++ src/test/platform/vdrs/upgrades.js
@@ -20,7 +20,7 @@ const snippets = tools.requirePayload(`${__dirname}/assets/snippets.json`);
 
 // NOTE - you will need the customerAdmin privilege to run these tests (to signup a new user with a new org)
 suite.forPlatform('vdrs', {payload: vdrSystem, schema}, test => {
-    let user, account, org, instance1, instance2;
+    let cloudWithUser, user, account, org, user2, account2, instance1, instance2, instance3;
     const closeioKey = 'closeio';
     beforeEach(() => {
         return cloud.post(`/customers/signup`, signup)
@@ -30,13 +30,28 @@ suite.forPlatform('vdrs', {payload: vdrSystem, schema}, test => {
                 org = r.body.organization;
                 // the default for a new org is still v1, eventually we will update this
                 expect(org.vdrVersion).to.equal('v1');
+                cloudWithUser = () => cloud.withOptions({ headers: { Authorization: `User ${user.secret}, Organization ${org.secret}` } });
             })
+            .then(() => {
+                const a = { name: `${tools.random()}churros`, externalId: `${tools.random()}@cloud-elements.com`};
+                return cloudWithUser().post(`/accounts`, a);
+            })
+            .then(r => account2 = r.body)
+            .then(() => {
+                const u = { email: `churros+rbac${tools.random()}@churros.com`, firstName: 'frank', lastName: 'ricard', password: 'Bingobango1!' };
+                return cloudWithUser().post(`/accounts/${account2.id}/users`, u);
+            })
+            .then(r => user2 = r.body)
+            .then(() => cloudWithUser().put(`/users/${user2.id}/roles/admin`))
             .then(() => defaults.withDefaults(user.secret, org.secret, user.email))
             .then(r => provisioner.create(closeioKey))
             .then(r => instance1 = r.body)
             .then(() => defaults.withDefaults(user.secret, org.secret, user.email))
             .then(r => provisioner.create(closeioKey))
             .then(r => instance2 = r.body)
+            .then(() => defaults.withDefaults(user2.secret, org.secret, user2.email))
+            .then(r => provisioner.create(closeioKey))
+            .then(r => instance3 = r.body)
             .then(() => defaults.reset())
             .catch(() => defaults.reset());
     });
@@ -45,9 +60,10 @@ suite.forPlatform('vdrs', {payload: vdrSystem, schema}, test => {
         return cloud.delete(`/customers/organizations/${org.id}`);
     });
 
-    const cloudWithUser = () => cloud.withOptions({ headers: { Authorization: `User ${user.secret}, Organization ${org.secret}` } });
+    const cloudWithUser2 = () => cloud.withOptions({ headers: { Authorization: `User ${user2.secret}, Organization ${org.secret}` } });
     const cloudWithInstance = () => cloud.withOptions({ headers: { Authorization: `User ${user.secret}, Organization ${org.secret}, Element ${instance1.token}`} });
     const cloudWithInstance2 = () => cloud.withOptions({ headers: { Authorization: `User ${user.secret}, Organization ${org.secret}, Element ${instance2.token}`} });
+    const cloudWithInstance3 = () => cloud.withOptions({ headers: { Authorization: `User ${user2.secret}, Organization ${org.secret}, Element ${instance3.token}`} });
 
     const genObj = path => {
         return {
@@ -213,6 +229,68 @@ suite.forPlatform('vdrs', {payload: vdrSystem, schema}, test => {
             .then(r => validateOrgAcctTransformExistsAndWorks());
     });
 
+    it('should support upgrade and rollback of my VDR version with account level VDRs in multiple accounts', () => {
+        let vdrId, transformationId;
+        const validateOrgAcctTransformExistsAndWorks = () => {
+            return cloudWithUser().get(`/organizations/objects/${vdrSystem.objectName}/definitions`, validateObject(1, ['vdrname']))
+                .then(r => vdrId = r.body.id)
+                .then(() => cloudWithUser().get(`/organizations/elements/${closeioKey}/transformations/${vdrSystem.objectName}`))
+                .then(r => transformationId = r.body.id)
+                // account 1
+                .then(() => cloudWithUser().get(`/accounts/${account.id}/objects/${vdrSystem.objectName}/definitions`, validateObject(1, ['vdrid'])))
+                .then(() => cloudWithUser().get(`/accounts/${account.id}/elements/${closeioKey}/transformations/${vdrSystem.objectName}`))
+                .then(() => cloudWithInstance().get(`hubs/crm/${vdrSystem.objectName}?pageSize=5`, r => {
+                    expect(r).to.have.statusCode(200);
+                    expect(r.body).to.not.be.empty;
+                    r.body.forEach(item => {
+                        expect(item.vdrname).to.not.be.undefined;
+                        expect(item.name).to.be.undefined;
+                        expect(item.vdrid).to.not.be.undefined;
+                        expect(item.id).to.be.undefined;
+                        expect(item.vdrtitle).to.be.undefined;
+                    })
+                }))
+                // account 2
+                .then(() => cloudWithUser2().get(`/accounts/${account2.id}/objects/${vdrSystem.objectName}/definitions`, validateObject(1, ['vdrtitle'])))
+                .then(() => cloudWithUser2().get(`/accounts/${account2.id}/elements/${closeioKey}/transformations/${vdrSystem.objectName}`))
+                .then(() => cloudWithInstance3().get(`hubs/crm/${vdrSystem.objectName}?pageSize=5`, r => {
+                    expect(r).to.have.statusCode(200);
+                    expect(r.body).to.not.be.empty;
+                    r.body.forEach(item => {
+                        expect(item.vdrname).to.not.be.undefined;
+                        expect(item.name).to.be.undefined;
+                        expect(item.vdrtitle).to.not.be.undefined;
+                        expect(item.title).to.be.undefined;
+                        expect(item.vdrid).to.be.undefined;
+                    })
+                }));
+        };
+    
+        // create some v1 objects (org)
+        return cloudWithUser().post(`/organizations/objects/${vdrSystem.objectName}/definitions`, genObj('name'), validateObject(1, ['vdrname']))
+            .then(r => cloudWithUser().post(`/organizations/elements/${closeioKey}/transformations/${vdrSystem.objectName}`, genTransform('name')))
+            // create some v1 objects (account 1)
+            .then(() => cloudWithUser().post(`/accounts/${account.id}/objects/${vdrSystem.objectName}/definitions`, genObj('id'), validateObject(1, ['vdrid'])))
+            .then(r => cloudWithUser().post(`/accounts/${account.id}/elements/${closeioKey}/transformations/${vdrSystem.objectName}`, genTransform('id')))
+            // create some v1 objects (account 2)
+            .then(() => cloudWithUser2().post(`/accounts/${account2.id}/objects/${vdrSystem.objectName}/definitions`, genObj('title'), validateObject(1, ['vdrtitle'])))
+            .then(r => cloudWithUser2().post(`/accounts/${account2.id}/elements/${closeioKey}/transformations/${vdrSystem.objectName}`, genTransform('title')))
+            // validate
+            .then(r => validateOrgAcctTransformExistsAndWorks())
+            // upgrade this org
+            .then(r => cloudWithUser().put(`/vdrs/upgrade/v2`, {}))
+            .then(r => cloudWithUser().get(`/organizations/me`, validateVdrVersion('v2')))
+            // validate everything in v2
+            .then(r => validateOrgAcctTransformExistsAndWorks())
+            .then(r => cloudWithUser().get(`/vdrs/${vdrId}`, validateObject(2, ['vdrname', 'vdrid'])))
+            .then(r => cloudWithUser().get(`/vdrs/${vdrId}/transformations/${transformationId}`))
+            // roll back
+            .then(r => cloudWithUser().delete(`/vdrs/upgrade/v2`))
+            .then(r => cloudWithUser().get(`/organizations/me`, validateVdrVersion('v1')))
+            // validate back in v1
+            .then(r => validateOrgAcctTransformExistsAndWorks());
+    });
+
     it('should support upgrade and rollback of my VDR version with org, account, and instance level VDRs', () => {
         let vdrId, transformationId;
         const validateOAITransformExistsAndWorks = () => {
@@ -303,4 +381,54 @@ suite.forPlatform('vdrs', {payload: vdrSystem, schema}, test => {
             // validate back in v1
             .then(r => validateOAITransformExistsAndWorks());
     });
+
+    it('should support upgrade and rollback of my VDR version with JS transformations', () => {
+        const transformWithJS = path => {
+            let t = genTransform(path);
+            t.script = {
+                body: "transformedObject.foo = 'bar'; done(transformedObject);"
+            }
+            return t;
+        }
+
+        let vdrId, transformationId;
+        const validateTransformExistsAndWorks = () => {
+            return cloudWithUser().get(`/organizations/objects/${vdrSystem.objectName}/definitions`, validateObject(1, ['vdrname']))
+                .then(r => vdrId = r.body.id)
+                .then(() => cloudWithUser().get(`/organizations/elements/${closeioKey}/transformations/${vdrSystem.objectName}`, r => {
+                    expect(r.body.script.body).to.contain('transformedObject.foo');
+                }))
+                .then(r => transformationId = r.body.id)
+                .then(() => cloudWithInstance().get(`hubs/crm/${vdrSystem.objectName}?pageSize=5`, r => {
+                    expect(r).to.have.statusCode(200);
+                    expect(r.body).to.not.be.empty;
+                    r.body.forEach(item => {
+                        expect(item.vdrname).to.not.be.undefined;
+                        expect(item.name).to.be.undefined;
+                        expect(item.foo).to.equal('bar');
+                    })
+                }));
+        };
+
+        // create some v1 objects
+        return cloudWithUser().post(`/organizations/objects/${vdrSystem.objectName}/definitions`, genObj('name'), validateObject(1, ['vdrname']))
+            .then(r => cloudWithUser().post(`/organizations/elements/${closeioKey}/transformations/${vdrSystem.objectName}`, transformWithJS('name')))
+            // validate
+            .then(r => validateTransformExistsAndWorks())
+            // upgrade this org
+            .then(r => cloudWithUser().put(`/vdrs/upgrade/v2`, {}))
+            .then(r => cloudWithUser().get(`/organizations/me`, validateVdrVersion('v2')))
+            // validate everything in v2
+            .then(r => validateTransformExistsAndWorks())
+            .then(r => cloudWithUser().get(`/vdrs/${vdrId}`, validateObject(1, ['vdrname'])))
+            .then(r => cloudWithUser().get(`/vdrs/${vdrId}/transformations/${transformationId}`, r =>{
+                expect(r.body.script.body).to.contain('transformedObject.foo');
+                expect(r.body.script.level).to.equal('organization');
+            }))
+            // roll back
+            .then(r => cloudWithUser().delete(`/vdrs/upgrade/v2`))
+            .then(r => cloudWithUser().get(`/organizations/me`, validateVdrVersion('v1')))
+            // validate again in v1
+            .then(r => validateTransformExistsAndWorks())
+        });
 });
