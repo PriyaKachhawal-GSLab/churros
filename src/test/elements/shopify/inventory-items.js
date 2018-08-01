3d0e360d91762adfb8dd25c5d7694e2b8c2f5b90
PriyaKachhawal-GSLab <priya.kachhawal@gslab.com>
2018-08-01 17:12:07 +0530

    Incorporated review comments

--- src/test/elements/shopify/inventory-items.js
+++ src/test/elements/shopify/inventory-items.js
@@ -10,10 +10,15 @@ suite.forElement('ecommerce', 'inventory-items', (test) => {
     "tracked": true
   });
 
-  let itemId;
+  let locationId, inventoryitemId, itemId;
   it('should allow Get and Patch for /hubs/marketing/inventory-items', () => {
-    return cloud.withOptions({ qs: { where: 'ids=2517889862' } }).get(`${test.api}`)
-      .then(r => itemId = r.body[0].id)
-      .then(r => cloud.patch(`${test.api}/${itemId}`, updatePayload()));
+    return cloud.get(`/locations`)
+    .then(r => locationId = r.body[0].id)
+    .then(r => cloud.get(`/locations/${locationId}/inventory-levels`))
+    .then(r => inventoryitemId = r.body[0].inventory_item_id)
+    .then(r => cloud.withOptions({ qs: { where: `ids='${inventoryitemId}'` } }).get(`${test.api}`))
+    .then(r => cloud.withOptions({ qs: { where: `ids='${inventoryitemId}'`, page : 1, pageSize: 1 } }).get(`${test.api}`))
+    .then(r => itemId = r.body[0].id)
+    .then(r => cloud.patch(`${test.api}/${itemId}`, updatePayload()));
   });
 });
