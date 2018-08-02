56f3d076663fceb0dc17fc7f872b27a64186f580
Sonali Soni <GS-1536@GSLAB.COM>
2018-08-02 10:32:18 +0530

    Added validation

--- src/test/elements/tangocard/orders.js
+++ src/test/elements/tangocard/orders.js
@@ -4,9 +4,10 @@ const suite = require('core/suite');
 const payload = require('core/tools').requirePayload(`${__dirname}/assets/orders.json`);
 const payload2 = require('core/tools').requirePayload(`${__dirname}/assets/orders2.json`);
 const cloud = require('core/cloud');
+const expect = require('chakram').expect;
 
 //Skipping the test since there are limits to the orders we can post and there are no delete API supported
-suite.forElement('rewards', 'orders', {payload: payload,  skip: true}, (test) => {
+suite.forElement('rewards', 'orders', {payload: payload}, (test) => {
  test.should.supportCrs();
  test.should.supportPagination();
 
@@ -18,6 +19,11 @@ suite.forElement('rewards', 'orders', {payload: payload,  skip: true}, (test) =>
     externalRefId = r.body.externalRefID;
    })
   .then(r => cloud.withOptions({ qs: { where: `externalRefID='${externalRefId}'` } }).get(test.api))
-   .then(r => cloud.post(`${test.api}/${orderId}/resends`));
+  .then(r => {
+   expect(r.body).to.not.be.empty;
+   expect(r.body.length).to.equal(1);
+   expect(r.body[0].externalRefID).to.equal(externalRefId);
+ })
+  .then(r => cloud.post(`${test.api}/${orderId}/resends`));
  });
 });
