79bc9afebeae2b07d016b5efd16bc2b413887461
Sonali Soni <GS-1536@GSLAB.COM>
2018-08-03 12:01:26 +0530

    Incorporated review comments

--- src/test/elements/tangocard/credit-cards.js
+++ src/test/elements/tangocard/credit-cards.js
@@ -3,40 +3,40 @@ const tools = require('core/tools');
 const cloud = require('core/cloud');
 const creditcardPayload = tools.requirePayload(`${__dirname}/assets/creditcard.json`);
 const payload2 = tools.requirePayload(`${__dirname}/assets/creditcard2.json`);
-const customerpayload = require('core/tools').requirePayload(`${__dirname}/assets/customersAccounts.json`);
-const payloadcustomer = require('core/tools').requirePayload(`${__dirname}/assets/customers.json`);
-const creditcarddeposits = require('core/tools').requirePayload(`${__dirname}/assets/creditcarddeposits.json`);
+const customerpayload = tools.requirePayload(`${__dirname}/assets/customersAccounts.json`);
+const payloadcustomer = tools.requirePayload(`${__dirname}/assets/customers.json`);
+const creditcarddeposits = tools.requirePayload(`${__dirname}/assets/creditcarddeposits.json`);
 
 //Skipping the test to POSt an credit-card we need to create customer first and customers object has limits of records to be create_discount_coupons
-suite.forElement('rewards', 'credit-cards', {skip: true}, (test) => {
- let accountId, customerId, depositId, id;
- it('should allow Csr for credit-cards and credit-cards-deposits', () => {
-  return cloud.post('/customers', payloadcustomer)
-   .then(r => customerId = r.body.id)
-   .then(r => cloud.post(`customers/${customerId}/accounts`, customerpayload))
-   .then(r => accountId = r.body.id)
-   .then(r => {
-    creditcardPayload.accountIdentifier = accountId;
-    creditcardPayload.customerIdentifier = customerId;
-   })
-   .then(r => cloud.post(`${test.api}`, creditcardPayload))
-   .then(r => {
-    id = r.body.id;
-   })
-   .then(r => cloud.get(`${test.api}/${id}`))
-   .then(r => {
-    payload2.accountIdentifier = accountId;
-    payload2.customerIdentifier = customerId;
-   })
-   .then(r => {
-    creditcarddeposits.accountIdentifier = accountId;
-    creditcarddeposits.customerIdentifier = customerId;
-   })
-   .then(r => cloud.post(`credit-cards/${id}/deposits`, creditcarddeposits))
-   .then(r => {
-    depositId = r.body.referenceDepositID;
-   })
-   .then(r => cloud.get(`credit-cards/deposits/${depositId}`))
-   .then(r => cloud.post(`${test.api}/${id}/unregister`, payload2));
- });
+suite.forElement('rewards', 'credit-cards', { skip: true }, (test) => {
+  let accountId, customerId, depositId, id;
+  it('should allow Csr for credit-cards and credit-cards-deposits', () => {
+    return cloud.post('/customers', payloadcustomer)
+      .then(r => customerId = r.body.id)
+      .then(r => cloud.post(`customers/${customerId}/accounts`, customerpayload))
+      .then(r => accountId = r.body.id)
+      .then(r => {
+        creditcardPayload.accountIdentifier = accountId;
+        creditcardPayload.customerIdentifier = customerId;
+      })
+      .then(r => cloud.post(`${test.api}`, creditcardPayload))
+      .then(r => {
+        id = r.body.id;
+      })
+      .then(r => cloud.get(`${test.api}/${id}`))
+      .then(r => {
+        payload2.accountIdentifier = accountId;
+        payload2.customerIdentifier = customerId;
+      })
+      .then(r => {
+        creditcarddeposits.accountIdentifier = accountId;
+        creditcarddeposits.customerIdentifier = customerId;
+      })
+      .then(r => cloud.post(`credit-cards/${id}/deposits`, creditcarddeposits))
+      .then(r => {
+        depositId = r.body.referenceDepositID;
+      })
+      .then(r => cloud.get(`credit-cards-deposits/${depositId}`))
+      .then(r => cloud.withOptions({ qs: { customerId: '${customerId}', accountId: '${accountId}' } }).delete(`${test.api}/${id}/unregister`));
+  });
 });
--- src/test/elements/tangocard/orders.js
+++ src/test/elements/tangocard/orders.js
@@ -1,29 +1,29 @@
 'use strict';
 
 const suite = require('core/suite');
-const payload = require('core/tools').requirePayload(`${__dirname}/assets/orders.json`);
-const payload2 = require('core/tools').requirePayload(`${__dirname}/assets/orders2.json`);
+const tools = require('core/tools');
+const payload = tools.requirePayload(`${__dirname}/assets/orders.json`);
+const payload2 = tools.requirePayload(`${__dirname}/assets/orders2.json`);
 const cloud = require('core/cloud');
 const expect = require('chakram').expect;
 
 //Skipping the test since there are limits to the orders we can post and there are no delete API supported
-suite.forElement('rewards', 'orders', {payload: payload, skip: true}, (test) => {
- test.should.supportCrs();
- test.should.supportPagination();
-
- let orderId,externalRefId;
- it('should support C for orders/{id}/resends', () => {
-  return cloud.post(test.api, payload2)
-   .then(r => {
-    orderId = r.body.id;
-    externalRefId = r.body.externalRefID;
-   })
-  .then(r => cloud.withOptions({ qs: { where: `externalRefID='${externalRefId}'` } }).get(test.api))
-  .then(r => {
-   expect(r.body).to.not.be.empty;
-   expect(r.body.length).to.equal(1);
-   expect(r.body[0].externalRefID).to.equal(externalRefId);
- })
-  .then(r => cloud.post(`${test.api}/${orderId}/resends`));
- });
+suite.forElement('rewards', 'orders', { payload: payload, skip: true }, (test) => {
+  test.should.supportCrs();
+  test.should.supportPagination();
+  it('should support C for orders/{id}/resends', () => {
+    let orderId, externalRefId;
+    return cloud.post(test.api, payload2)
+      .then(r => {
+        orderId = r.body.id;
+        externalRefId = r.body.externalRefID;
+      })
+      .then(r => cloud.withOptions({ qs: { where: `externalRefID='${externalRefId}'` } }).get(test.api))
+      .then(r => {
+        expect(r.body).to.not.be.empty;
+        expect(r.body.length).to.equal(1);
+        expect(r.body[0].externalRefID).to.equal(externalRefId);
+      })
+      .then(r => cloud.post(`${test.api}/${orderId}/resends`));
+  });
 });
