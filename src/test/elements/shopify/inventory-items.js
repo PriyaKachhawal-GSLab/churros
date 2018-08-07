174ef4ffa18c396e0f2d73bf7300f14da276dfe2
PriyaKachhawal-GSLab <priya.kachhawal@gslab.com>
2018-08-07 18:25:49 +0530

    Incorporated review comments

--- src/test/elements/shopify/inventory-items.js
+++ src/test/elements/shopify/inventory-items.js
@@ -2,23 +2,18 @@
 
 const suite = require('core/suite');
 const cloud = require('core/cloud');
+const payload = require('./assets/inventory-items.json');
 
 suite.forElement('ecommerce', 'inventory-items', (test) => {
-
-  const updatePayload = () => ({
-    "sku": "new sku",
-    "tracked": true
-  });
-
-  let locationId, inventoryitemId, itemId;
   it('should allow Get and Patch for /hubs/marketing/inventory-items', () => {
+    let locationId, inventoryitemId, itemId;
     return cloud.get(`/locations`)
-    .then(r => locationId = r.body[0].id)
-    .then(r => cloud.get(`/locations/${locationId}/inventory-levels`))
-    .then(r => inventoryitemId = r.body[0].inventory_item_id)
-    .then(r => cloud.withOptions({ qs: { where: `ids='${inventoryitemId}'` } }).get(`${test.api}`))
-    .then(r => cloud.withOptions({ qs: { where: `ids='${inventoryitemId}'`, page : 1, pageSize: 1 } }).get(`${test.api}`))
-    .then(r => itemId = r.body[0].id)
-    .then(r => cloud.patch(`${test.api}/${itemId}`, updatePayload()));
+      .then(r => locationId = r.body[0].id)
+      .then(r => cloud.get(`/locations/${locationId}/inventory-levels`))
+      .then(r => inventoryitemId = r.body[0].inventory_item_id)
+      .then(r => cloud.withOptions({ qs: { where: `ids='${inventoryitemId}'` } }).get(`${test.api}`))
+      .then(r => cloud.withOptions({ qs: { where: `ids='${inventoryitemId}'`, page: 1, pageSize: 1 } }).get(`${test.api}`))
+      .then(r => itemId = r.body[0].id)
+      .then(r => cloud.patch(`${test.api}/${itemId}`, payload));
   });
 });
--- src/test/elements/shopify/locations.js
+++ src/test/elements/shopify/locations.js
@@ -2,33 +2,38 @@
 
 const suite = require('core/suite');
 const cloud = require('core/cloud');
+const expect = require('chakram').expect;
+const inventoryItemPayload = require('./assets/inventory-items.json');
+const inventorylevelPayload = require('./assets/inventory-levels.json');
+const updateInventoryLevelpayload = require('./assets/updateInventoryLevels.json');
 
 suite.forElement('ecommerce', 'locations', (test) => {
+  let locationId, itemId;
+  before(() =>
+    cloud.get(`${test.api}`)
+    .then(r => locationId = r.body[0].id)
+    .then(r => cloud.get(`${test.api}/${locationId}/inventory-levels`))
+    .then(r => itemId = r.body[0].inventory_item_id));
 
-  const createPayload = () => ({
-      "available": 42
-  });
-
-  const updatePayload = () => ({
-      "available_adjustment": 5
-  });
-
-  const updateInventoryItemPayload = () => ({
-    "tracked": true
-  });
-
-  let locationId,itemId;
-  it('should allow CRUDS for /hubs/marketing/locations', () => {
-    return cloud.get(`${test.api}`)
-      .then(r => locationId = r.body[0].id)
-      .then(r => cloud.get(`${test.api}/${locationId}/inventory-levels`))
-      .then(r => cloud.withOptions({ qs: {page : 1, pageSize: 1 } }).get(`${test.api}/${locationId}/inventory-levels`))
-      .then(r => itemId = r.body[0].inventory_item_id)
-      .then(r => cloud.get(`${test.api}/${locationId}/inventory-items/${itemId}/inventory-levels`))
-      .then(r => cloud.withOptions({ qs: {page : 1, pageSize: 1 } }).get(`${test.api}/${locationId}/inventory-items/${itemId}/inventory-levels`))
-      .then(r => cloud.patch(`inventory-items/${itemId}`, updateInventoryItemPayload()))
-      .then(r => cloud.patch(`${test.api}/${locationId}/inventory-items/${itemId}/inventory-levels`, updatePayload()))
-      .then(r => cloud.post(`${test.api}/${locationId}/inventory-items/${itemId}/inventory-levels`, createPayload()))
+  it('should allow CRU for /hubs/marketing/locations/${id}/inventory-items/${itemId}/inventory-levels', () => {
+    return cloud.get(`${test.api}/${locationId}/inventory-items/${itemId}/inventory-levels`)
+      .then(r => cloud.patch(`inventory-items/${itemId}`, inventoryItemPayload))
+      .then(r => cloud.patch(`${test.api}/${locationId}/inventory-items/${itemId}/inventory-levels`, updateInventoryLevelpayload))
+      .then(r => cloud.post(`${test.api}/${locationId}/inventory-items/${itemId}/inventory-levels`, inventorylevelPayload))
       .then(r => cloud.post(`${test.api}/${locationId}/inventory-items/${itemId}/inventory-levels`));
+  });
+
+  it(`should support pagination for ${test.api}/{id}/inventory-levels`, () => {
+    return cloud.withOptions({ qs: { page: 1, pageSize: 1 } }).get(`${test.api}/${locationId}/inventory-levels`)
+      .then(r => {
+        expect(r.body.length).to.equal(1);
+      });
+  });
+
+  it(`should support pagination for ${test.api}/{id}/inventory-items/${itemId}/inventory-levels`, () => {
+    return cloud.withOptions({ qs: { page: 1, pageSize: 1 } }).get(`${test.api}/${locationId}/inventory-items/${itemId}/inventory-levels`)
+      .then(r => {
+        expect(r.body.length).to.equal(1);
+      });
   });
 });
