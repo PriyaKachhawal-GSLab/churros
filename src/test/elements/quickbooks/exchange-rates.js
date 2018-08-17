8f9cd61410ed4c468bb77fd4ccee9213f65dfc72
Mithila Garud <mithila.garud@gslab.com>
2018-08-17 14:38:18 +0530

    Test cases for new resources on QBO

---
+++ src/test/elements/quickbooks/exchange-rates.js
@@ -0,0 +1,16 @@
+'use strict';
+
+const suite = require('core/suite');
+const tools = require('core/tools');
+const cloud = require('core/cloud');
+const expect = require('chakram').expect;
+const payload = tools.requirePayload(`${__dirname}/assets/exchange-rates.json`);
+
+suite.forElement('finance', 'entitlements', { payload: payload }, (test) => {
+  it(`Should support RU on ${test.api}`, () => {
+    cloud.get(`${test.api}/USD`)
+      .then(r => expect(r).to.statusCode(200));
+    //PUT only works with account having multicurrency enabled. Hence need to skip
+    cloud.withOptions({ skip: true }).put(`${test.api}/USD`, payload);
+  });
+});
--- src/test/elements/quickbooks/journal-codes.js
+++ src/test/elements/quickbooks/journal-codes.js
@@ -2,37 +2,32 @@
 
 const suite = require('core/suite');
 const tools = require('core/tools');
+const cloud = require('core/cloud');
 const chakram = require('chakram');
 const expect = chakram.expect;
-const cloud = require('core/cloud');
-const payload = tools.requirePayload(`${__dirname}/assets/credit-terms.json`);
+const payload = tools.requirePayload(`${__dirname}/assets/journal-codes.json`);
 
-//Need to skip as there is no delete API
-suite.forElement('finance', 'credit-terms', { payload: payload }, (test) => {
-  let termId;
+//Need to skip as the resource  only works in the France locale on QBO
+suite.forElement('finance', 'journal-codes', { payload: payload, skip: true }, (test) => {
+  let entityId;
   let putPayload;
   test.should.supportSr();
   test.withOptions({ skip: true }).should.return200OnPost();
   it(`should support update on ${test.api}`, () => {
     return cloud.get(test.api)
       .then(r => {
-        termId = r.body[0].id;
+        entityId = r.body[0].id;
         putPayload = r.body[0];
         delete putPayload.id;
-        delete putPayload.attachableRef;
         delete putPayload.metaData;
       })
       .then(cloud.put(`${test.api}/${termId}`, putPayload));
   });
   test.withOptions({ qs: { page: 1, pageSize: 5 } }).should.return200OnGet();
-  test.withName(`should support searching ${test.api} by Id and returnCount in response`)
-    .withOptions({ qs: { where: `id ='1234'`, returnCount: true } })
+  test.withName(`should support searching ${test.api} by Name`)
+    .withOptions({ qs: { where: `Name ='VT'` } })
     .withValidation((r) => {
       expect(r).to.have.statusCode(200);
-      const validValues = r.body.filter(obj => obj.id = '1234');
-      expect(validValues.length).to.equal(r.body.length);
-      expect(r.response.headers['elements-total-count']).to.exist;
+      const validValues = r.body.filter(obj => obj.Name = 'VT');
     }).should.return200OnGet();
-
-
 });
