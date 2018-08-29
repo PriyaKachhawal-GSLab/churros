0d9f1a06512bf4466b570983f8b96076dc779c2b
Isha B Deshpande <gsc-30169@GSLAB.COM>
2018-08-29 12:25:19 +0530

    used should.supportCrd()

--- src/test/elements/jira/agents.js
+++ src/test/elements/jira/agents.js
@@ -5,14 +5,8 @@ const tools = require('core/tools');
 const cloud = require('core/cloud');
 const payload = tools.requirePayload(`${__dirname}/assets/agents.json`);
 
-suite.forElement('helpdesk', 'agents', (test) => {
-  it(`should allow CRDS for ${test.api} with Ceql search`, () => {
-    let agentId;
-    return cloud.post(test.api, payload)
-      .then(r => agentId = r.body.key)
-      .then(r => cloud.get(`${test.api}/${agentId}`))
-      .then(r => cloud.withOptions({ qs: {where:`username='jiradev'`}}).get(test.api))
-      .then(r => cloud.delete(`${test.api}/${agentId}`));
-   });
-test.withOptions({ qs: {where:`username='jiradev'`}}).should.supportPagination();
+suite.forElement('helpdesk', 'agents',{payload:payload}, (test) => {
+  test.should.supportCrd();
+  test.withOptions({ qs: {where:`username='jiradev'`}}).should.supportPagination();
+  test.withName('should support Ceql search').withOptions({ qs: {where:`username='jiradev'`} }).should.return200OnGet();
 });
