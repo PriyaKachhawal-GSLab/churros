657a8d1ababda0801007dcba399bbd50cacaca18
Akash R Singh <GS-1670@GSLAB.COM>
2018-08-29 17:22:54 +0530

    Zuorav2 tests added for two product-rate-plans apis

--- src/test/elements/zuorav2/products.js
+++ src/test/elements/zuorav2/products.js
@@ -4,6 +4,7 @@ const suite = require('core/suite');
 const payload = require('./assets/products');
 const chakram = require('chakram');
 const tools = require('core/tools');
+const cloud = require('core/cloud');
 const build = (overrides) => Object.assign({}, payload, overrides);
 const accountsPayload = build({ Name: tools.random(), SKU: tools.random() });
 
@@ -18,4 +19,11 @@ suite.forElement('payment', 'products', { payload: accountsPayload }, (test) => 
   test.withOptions(options).should.supportCruds(chakram.put);
   test.should.supportNextPagePagination(2);
   test.should.supportCeqlSearch('id');
+
+  it('should allow S for /hubs/payment/products/{id}/product-rate-plans', () => {
+    let productId = -1;
+    return cloud.get(test.api)
+      .then(r => productId = r.body[0].id)
+      .then(r => cloud.withOptions({ qs: { page: 1, pageSize: 1 } }).get(`${test.api}/${productId}/product-rate-plans`));
+  });
 });
