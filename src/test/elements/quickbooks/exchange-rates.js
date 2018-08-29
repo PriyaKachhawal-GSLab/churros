3b7ca068bae3c5270617df6caad056bbe8f17e24
GarimaAgarwal-GSLab <garima.agarwal@gslab.com>
2018-08-29 11:31:46 +0530

    corrected test as per review comments

--- src/test/elements/quickbooks/exchange-rates.js
+++ src/test/elements/quickbooks/exchange-rates.js
@@ -7,10 +7,13 @@ const expect = require('chakram').expect;
 const payload = tools.requirePayload(`${__dirname}/assets/exchange-rates.json`);
 
 suite.forElement('finance', 'exchange-rates', { payload: payload }, (test) => {
-  it(`Should support RU on ${test.api}`, () => {
-    cloud.get(`${test.api}/USD`)
+  it(`Should support R on ${test.api}`, () => {
+    return cloud.get(`${test.api}/USD`)
       .then(r => expect(r).to.statusCode(200));
-    //PUT only works with account having multicurrency enabled. Hence need to skip
-    cloud.withOptions({ skip: true }).put(`${test.api}/USD`, payload);
+  });
+  //PUT only works with account having multicurrency enabled. Hence need to skip
+  it.skip(`Should support U on ${test.api}`, () => {
+    return cloud.put(`${test.api}/USD`, payload)
+      //.then(r => cloud.withOptions({ skip: true }).put(`${test.api}/USD`, payload));
   });
 });
