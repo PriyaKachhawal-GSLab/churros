490ef3a6eb5a1cae60d333c05218eeba492f81dd
Vimlesh Gupta <vimlesg.gupta@gslab.com>
2018-08-30 18:33:20 +0530

    fixed cci errors

--- src/test/elements/fortnox/voucher-series.js
+++ src/test/elements/fortnox/voucher-series.js
@@ -1,8 +1,6 @@
 'use strict';
 
 const suite = require('core/suite');
-const tools = require('core/tools');
-const cloud = require('core/cloud');
 
 suite.forElement('erp', 'voucher-series', (test) => {
   test.withApi(`${test.api}`)
