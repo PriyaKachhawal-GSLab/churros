530ee32c61709b563113f45880c8eb8f02adfb08
kallurisandeep <sandeepcelements@gmail.com>
2018-09-05 11:29:18 -0500

    added aditional checks

--- src/test/elements/maximizer/leads.js
+++ src/test/elements/maximizer/leads.js
@@ -3,7 +3,17 @@
 const suite = require('core/suite');
 const tools = require('core/tools');
 const cloud = require('core/cloud');
+const expect = require('chakram').expect;
 
 suite.forElement('crm', 'leads', (test) => {
+    let FirstName='MyFirstName'
     test.should.supportS();
+    test.should.supportPagination();
+    test.withApi(test.api)
+      .withOptions({ qs: { where: `FirstName='${FirstName}'`, fields: `Email.Description,SecStatus.CanUpdate` } })
+      .withValidation(r => {
+         expect(r.body.filter(obj => obj.FirstName === `${FirstName}`).length).to.equal(r.body.length);
+         expect(Object.keys(r.body[0]).length).to.equal(2);
+      });
 });
+
