9a9dde90985d020b3de9e7b4d47dba5c67da2ae9
kallurisandeep <sandeepcelements@gmail.com>
2018-09-05 18:00:26 -0500

    modified test cases

--- src/test/elements/maximizer/leads.js
+++ src/test/elements/maximizer/leads.js
@@ -6,14 +6,15 @@ const cloud = require('core/cloud');
 const expect = require('chakram').expect;
 
 suite.forElement('crm', 'leads', (test) => {
-    let FirstName='MyFirstName'
+    let FirstName='MyFirstName';
     test.should.supportS();
     test.should.supportPagination();
     test.withApi(test.api)
+      .withName(`should support leads filter by FirstName='${FirstName}'`)
       .withOptions({ qs: { where: `FirstName='${FirstName}'`, fields: `Email.Description,SecStatus.CanUpdate` } })
       .withValidation(r => {
-         expect(r.body.filter(obj => obj.FirstName === `${FirstName}`).length).to.equal(r.body.length);
-         expect(Object.keys(r.body[0]).length).to.equal(2);
-      });
+         expect(r.body.filter(obj => obj.FirstName === `${FirstName}`));
+         expect(Object.keys(r.body[0]).length).to.equal(2);})
+      .should.return200OnGet();;
 });
 
