e84493f2c2457eb8dad107de5212c9bb9e90a5fb
srisatyadev567@gmail.com <srisatyadev567@gmail.com>
2018-09-06 22:59:11 -0500

    standarizing churros assests for marketo

--- src/test/elements/marketo/external-activity.js
+++ src/test/elements/marketo/external-activity.js
@@ -2,10 +2,9 @@
 
 const suite = require('core/suite');
 const tools = require('core/tools');
-const attributePayload = tools.requirePayload(`${__dirname}/assets/attributes-create.json`);
-const activityPayload = tools.requirePayload(`${__dirname}/assets/externalActivity-update.json`);
+const attributePayload = tools.requirePayload(`${__dirname}/assets/externalActivityAttributes-create.json`);
+const activityPayload = tools.requirePayload(`${__dirname}/assets/externalActivity-create.json`);
 const cloud = require('core/cloud');
-
 /*
 Need different permissions for external-activities (ready-talk's sandbox)
     "username": "developer@cloud-elements.com",
@@ -13,7 +12,6 @@ Need different permissions for external-activities (ready-talk's sandbox)
     "marketo.identity.url": "https://338-ZRW-745.mktorest.com/identity",
     "base.url": "https://338-ZRW-745.mktorest.com/rest",
 */
-
 suite.forElement('marketing', 'external-activity', { payload: activityPayload, skip: true }, (test) => {
   it('should allow CS external-activity/types, PATCH external-activity/type/{apiName}/approve and PATCH external-activity/type/{apiName}/attributes ', () => {
     let apiName;
