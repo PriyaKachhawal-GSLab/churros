46ff24d2b791502578b63c66215b7e6a3d6133b3
Taylor Clark <taylor@cloud-elements.com>
2018-09-10 01:49:12 +0300

    removed unused import

--- src/test/elements/eloqua/activity-types.js
+++ src/test/elements/eloqua/activity-types.js
@@ -1,7 +1,6 @@
 'use strict';
 
 const suite = require('core/suite');
-const cloud = require('core/cloud');
 
 suite.forElement('marketing', 'activity-types', null, (test) => {
   test.should.return200OnGet();
