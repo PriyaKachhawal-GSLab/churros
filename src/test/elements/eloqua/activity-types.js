ffb5e17563e75f3726f1e8b490e750355d562c9d
Taylor Clark <taylor@cloud-elements.com>
2018-09-10 01:47:32 +0300

    moved test over to own file

--- src/test/elements/eloqua/activity-types.js
+++ src/test/elements/eloqua/activity-types.js
@@ -5,7 +5,4 @@ const cloud = require('core/cloud');
 
 suite.forElement('marketing', 'activity-types', null, (test) => {
   test.should.return200OnGet();
-  it('should allow ping for eloqua', () => {
-    return cloud.get(`/hubs/marketing/ping`);
-  });
 });
