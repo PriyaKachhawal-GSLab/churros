7b9efccd829da3eca24f87e93f367cc2d44e6f64
Naga Seshu Babu Ede <seshuede@gmail.com>
2018-09-08 22:14:50 -0500

    updated payloads to snake case

--- src/test/elements/zendesk/sla-policies.js
+++ src/test/elements/zendesk/sla-policies.js
@@ -3,8 +3,8 @@
 const tools = require('core/tools');
 const suite = require('core/suite');
 
-const slaPoliciesCreatePayload = tools.requirePayload(`${__dirname}/assets/slaPolicies-create.json`);
-const slaPoliciesUpdatePayload = tools.requirePayload(`${__dirname}/assets/slaPolicies-update.json`);
+const slaPoliciesCreatePayload = tools.requirePayload(`${__dirname}/assets/sla-policies-create.json`);
+const slaPoliciesUpdatePayload = tools.requirePayload(`${__dirname}/assets/sla-policies-update.json`);
 
 const options = {
  churros: {
