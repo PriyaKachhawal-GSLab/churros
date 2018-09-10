21272f5c7b5c9e9c2cc75d7e8e9a1828672794b3
Sonali Soni <GS-1536@GSLAB.COM>
2018-09-10 13:04:43 +0530

    Fixed Circle Ci

--- src/test/elements/dropboxbusiness/namespaces.js
+++ src/test/elements/dropboxbusiness/namespaces.js
@@ -4,7 +4,6 @@ const suite = require('core/suite');
 const props = require('core/props');
 
 suite.forElement('documents', 'namespaces', (test) => {
-  let memberId = props.getForKey('dropboxbusiness', 'username');
   test.should.supportS();
   test.should.supportPagination();
 });
