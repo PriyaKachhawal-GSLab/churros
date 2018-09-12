976f44a874e62a63dcedd498019f62a1f1938fcc
kallurisandeep <sandeepcelements@gmail.com>
2018-09-12 15:41:19 -0500

    added pagination test for sfdc

--- src/test/elements/sfdc/report-types.js
+++ src/test/elements/sfdc/report-types.js
@@ -2,8 +2,6 @@
 
 
 const suite = require('core/suite');
-const tools = require('core/tools');
-const cloud = require('core/cloud');
 
 suite.forElement('crm', 'report-types', (test) => {
     test.should.supportPagination();
