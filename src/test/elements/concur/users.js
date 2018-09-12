0d273200f371948752c9a46d1972d1f7516b3546
kallurisandeep <sandeepcelements@gmail.com>
2018-09-12 12:21:10 -0500

    added test for users

--- src/test/elements/concur/users.js
+++ src/test/elements/concur/users.js
@@ -1,18 +1,37 @@
 'use strict';
 
+const tools = require('core/tools');
 const suite = require('core/suite');
+const cloud = require('core/cloud');
 const expect = require('chakram').expect;
+const createpayload = tools.requirePayload(`${__dirname}/assets/users-create.json`);
+const updatePayload = tools.requirePayload(`${__dirname}/assets/users-update.json`);
 
-suite.forElement('expense', 'users', (test) => {
-  test.should.return200OnGet();
-  test.should.supportNextPagePagination(2);
-  test
-    .withName(`should support searching ${test.api} by Active`)
-    .withOptions({ qs: { where: 'active = \'true\'' } })
-    .withValidation((r) => {
-      expect(r).to.have.statusCode(200);
-      const validValues = r.body.filter(obj => obj.Active === true);
-      expect(validValues.length).to.equal(r.body.length);
-    }).should.return200OnGet();
+suite.forElement('finance', 'users', { payload: createpayload }, (test) => {
 
+  it('should support CRUS, pagination and Ceql searching for ${test.api}', () => {
+    let id, ID, email;
+    return cloud.post(test.api, createpayload)
+     
+      .then(r => cloud.get(test.api))
+      .then (r => {
+        email = r.body[0].PrimaryEmail;
+        ID = r.body[0].LoginID;
+        id = r.body[0].EmployeeID;
+      })
+      .then(r => cloud.withOptions({ qs: { where: `PrimaryEmail='${email}'` } }).get(test.api))
+      .then(r => expect(r.body.filter(o => o.PrimaryEmail === `${email}`)).to.not.be.empty)
+      .then(r => cloud.get(`${test.api}/${ID}`))
+      .then(r => {
+        updatePayload.EmployeeID = id;
+        updatePayload.LoginId = ID;
+        
+      })
+      .then(r => cloud.put(`${test.api}/${id}`, updatePayload));
+  });
+  test.should.supportPagination('id');
+  
 });
+
+
+
