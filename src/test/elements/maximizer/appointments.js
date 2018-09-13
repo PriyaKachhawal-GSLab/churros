a8cc9825a440e83242054d6b1abc94b62243a61a
kallurisandeep <sandeepcelements@gmail.com>
2018-09-13 11:54:01 -0500

    removed comments

--- src/test/elements/maximizer/appointments.js
+++ src/test/elements/maximizer/appointments.js
@@ -32,22 +32,6 @@ suite.forElement('crm', 'appointments', { payload : appointmentPayload }, (test)
       })
       .should.return200OnGet();
 
-      // test.withApi(test.api)
-      // .withName(`should support nested search i.e.,  filter by where with multiple options using and`)
-      // .withOptions({ qs: { where: `Users.Key.UID='MASTER'' and StartDate='2015-03-09T20:00:00Z'` } })
-      // .withValidation(r => {
-      //   expect(r.body.filter(obj => obj.Users[0].Key.UID === `MASTER`));
-      // })
-      // .should.return200OnGet();
-
-      // test.withApi(test.api)
-      // .withName(`should support nested search i.e.,  filter by where with multiple options using or`)
-      // .withOptions({ qs: { where: `Users.Key.UID='MASTER'' or StartDate='2015-03-09T20:00:00Z'` } })
-      // .withValidation(r => {
-      //   expect(r.body.filter(obj => obj.Users[0].Key.UID === `MASTER`));
-      // })
-      // .should.return200OnGet();
-
   test.withApi(test.api)
       .withOptions({ qs: { where: `Subject='${appointmentPayload.Subject}'`, fields: `Subject,StartDate` } })
       .withValidation(r => {
