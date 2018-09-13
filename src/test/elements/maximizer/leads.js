fd62cc666aa4665b42cd90d9c604fe45dd12d039
kallurisandeep <sandeepcelements@gmail.com>
2018-09-13 11:20:00 -0500

    added new checks for leads

--- src/test/elements/maximizer/leads.js
+++ src/test/elements/maximizer/leads.js
@@ -1,18 +1,31 @@
 'use strict';
 
 const suite = require('core/suite');
+const tools = require('core/tools');
+const cloud = require('core/cloud');
+const faker = require('faker');
 const expect = require('chakram').expect;
 
-suite.forElement('crm', 'leads', (test) => {
-    let FirstName='MyFirstName';
-    test.should.supportS();
-    test.should.supportPagination();
-    test.withApi(test.api)
-      .withName(`should support leads filter by FirstName='${FirstName}'`)
-      .withOptions({ qs: { where: `FirstName='${FirstName}'`, fields: `Email.Description,SecStatus.CanUpdate` } })
-      .withValidation(r => {
-         expect(r.body.filter(obj => obj.FirstName === `${FirstName}`));
-         expect(Object.keys(r.body[0]).length).to.equal(2);})
-      .should.return200OnGet();
+const createPayload = tools.requirePayload(`${__dirname}/assets/leads-create.json`);
+const updatePayload = tools.requirePayload(`${__dirname}/assets/leads-update.json`);
+
+suite.forElement('crm', 'leads', { payload: createPayload}, (test) => {
+  it('should support CRUDS, pagination for /hubs/crm/leads', () => {
+    let id, name;
+    return cloud.post(test.api, createPayload)
+      .then(r => {
+        id = r.body.Key;
+        name = r.body.FirstName;
+      })
+      .then(r => cloud.get(test.api))
+      .then(r => cloud.withOptions({ qs: { page: 1, pageSize: 1 } }).get(test.api))
+      .then(r => cloud.withOptions({ qs: { where: `FirstName='${name}'` } }).get(test.api))
+      .then(r => expect(r.body.filter(o => o.FirstName === `${name}`)).to.not.be.empty)
+      .then(r => cloud.get(`${test.api}/${id}`))
+      .then(r => cloud.patch(`${test.api}/${id}`, updatePayload))
+      // deleted test is in beta version(vendor mes:This functionality is currently of BETA/CTP quality (not ready for production usage) 
+      //.then(r => cloud.delete(`${test.api}/${id}`));
+  });
+  test.should.supportPagination('id');
 });
 
