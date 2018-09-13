5b446be1f37a8ae655167bd7d631614b12cb01a8
srisatyadev567@gmail.com <srisatyadev567@gmail.com>
2018-09-13 18:12:04 -0500

    churros update

--- src/test/elements/sfdcservicecloud/agents.js
+++ src/test/elements/sfdcservicecloud/agents.js
@@ -3,7 +3,6 @@
 const suite = require('core/suite');
 const tools = require('core/tools');
 const cloud = require('core/cloud');
-const build = (overrides) => Object.assign({}, payload, overrides);
 const agentsPayload = tools.requirePayload(`${__dirname}/assets/agents-create.json`);
 const agentsUpdatePayload = tools.requirePayload(`${__dirname}/assets/agents-update.json`);
 
