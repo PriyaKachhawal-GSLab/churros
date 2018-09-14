fc849f4e61f150a5bab3a512690d1b8f372c5877
Abhishek Khanna <GSC-30271@GSLAB.COM>
2018-09-14 19:17:36 +0530

    updated filenames

--- src/test/elements/sugarcrmv2/activities.js
+++ src/test/elements/sugarcrmv2/activities.js
@@ -3,7 +3,7 @@
 const suite = require('core/suite');
 const payload = require('./assets/activities-create.json');
 const updatePayload = require('./assets/activities-update.json');
-const queryTypePayload = require('./assets/activities-QueryTest.json');
+const queryTypePayload = require('./assets/activities-requiredQueryParam-crud.json');
 const chakram = require('chakram');
 
 suite.forElement('crm', 'activities', { payload: payload }, (test) => {
