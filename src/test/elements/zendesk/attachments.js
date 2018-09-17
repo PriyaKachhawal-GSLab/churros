a7901169b44d444bdbb6ad82122f01a710b6123f
Taylor Clark <taylor@cloud-elements.com>
2018-09-17 05:10:11 +0200

    fixed attachments required query param

--- src/test/elements/zendesk/attachments.js
+++ src/test/elements/zendesk/attachments.js
@@ -2,12 +2,12 @@
 
 const suite = require('core/suite');
 const cloud = require('core/cloud');
+const attachmentQuery =  tools.requirePayload(`${__dirname}/assets/attachments-requiredQueryParam-c.json`);
 
 suite.forElement('helpdesk', 'attachments', null, (test) => {
   it('should support Crd for attachments', () => {
     let attachmentId;
-    let query = { fileName: "attach.txt" };
-    return cloud.withOptions({ qs: query }).postFile(test.api, __dirname + '/assets/attachments-create.txt')
+    return cloud.withOptions({ qs: attachmentQuery }).postFile(test.api, __dirname + '/assets/attachments-create.txt')
       .then(r => attachmentId = r.body.id)
       .then(r => cloud.get(`${test.api}/${attachmentId}`))
       .then(r => cloud.delete(`${test.api}/${attachmentId}`));
